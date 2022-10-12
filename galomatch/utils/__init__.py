358c985e16631700c6f5005d65d711ce7f1bdb04
rstiskalek <richard.stiskalek@protonmail.com>
2022-10-12 16:56:48 +0100

    add recarray manipulations

--- galomatch/utils/__init__.py
+++ galomatch/utils/__init__.py
@@ -13,4 +13,4 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-from galomatch import (io, match, utils)
+from .recarray_manip import (add_columns, rm_columns)
