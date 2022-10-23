ab3bf0ffaf80c6964b3c679f8c30e9e34f5a9502
rstiskalek <richard.stiskalek@protonmail.com>
2022-10-23 16:22:47 +0100

    add import

--- csiborgtools/fits/__init__.py
+++ csiborgtools/fits/__init__.py
@@ -13,5 +13,5 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-from .haloprofile import (nfw_profile, nfw_mass)  # noqa
+from .haloprofile import (NFWProfile, NFWPosterior)  # noqa
 from .halofits  import distribute_halos  # noqa
