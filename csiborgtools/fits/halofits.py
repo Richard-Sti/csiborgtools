c1cc3903f817bc69b2a90eafcd499a120ca7eb28
rstiskalek <richard.stiskalek@protonmail.com>
2022-10-28 22:11:00 +0100

    add halo dump

--- csiborgtools/fits/halofits.py
+++ csiborgtools/fits/halofits.py
@@ -18,10 +18,12 @@ Tools for fitting the halos and distributing the jobs.
 
 
 import numpy
+from os.path import join
+from tqdm import trange
 from ..io import nparts_to_start_ind
 
 
-def clump_with_particles(particle_clumps, clump_indxs, verbose=True):
+def clump_with_particles(particle_clumps, clumps, verbose=True):
     """
     Count how many particles does each clump have.
 
@@ -29,8 +31,8 @@ def clump_with_particles(particle_clumps, clump_indxs, verbose=True):
     ----------
     particle_clumps : 1-dimensional array
         Array of particles' clump IDs.
-    clump_indxs : 1-dimensional array
-        Array of clump indices (not particles's clump IDs!).
+    clumps : structured array
+        The clumps array.
 
     Returns
     -------
@@ -42,55 +44,87 @@ def clump_with_particles(particle_clumps, clump_indxs, verbose=True):
     unique_particle_clumps = numpy.unique(particle_clumps)
     if verbose:
         print("Checking which clumps have particles...")
-    return numpy.isin(clump_indxs, unique_particle_clumps)
+    return numpy.isin(clumps["index"], unique_particle_clumps)
 
 
-def distribute_halos(Njobs, clump_indxs):
+def distribute_halos(Nsplits, clumps):
     """
-    Evenly distribute clump indices to CPUs. `clump_indxs` should be only of
-    the clumps that contain particles.
+    Evenly distribute clump indices to smaller splits. Clumps should only be
+    clumps that contain particles.
 
     Parameters
     ----------
-    Njobs : int
-        Number of jobs.
-    clump_indxs : 1-dimensional array
-        Array of clump indices.
+    Nsplits : int
+        Number of splits.
+    clumps : structured array
+        The clumps array.
 
     Returns
     -------
-    start : 1-dimensional array
-        The starting clump index of each CPU.
-    end: 1-dimensional array
-        The final clump index of each CPU.
+    splits : 2-dimensional array
+        Array of starting and ending indices of each CPU of shape `(Njobs, 2)`.
     """
     # Make sure these are unique IDs
-    if clump_indxs.size > numpy.unique((clump_indxs)).size:
+    indxs = clumps["index"]
+    if indxs.size > numpy.unique((indxs)).size:
         raise ValueError("`clump_indxs` constains duplicate indices.")
-    Ntotal = clump_indxs.size
-    Njobs_per_cpu = numpy.ones(Njobs, dtype=int) * Ntotal // Njobs
+    Ntotal = indxs.size
+    Njobs_per_cpu = numpy.ones(Nsplits, dtype=int) * Ntotal // Nsplits
     # Split the remainder Ntotal % Njobs among the CPU
-    Njobs_per_cpu[:Ntotal % Njobs] += 1
+    Njobs_per_cpu[:Ntotal % Nsplits] += 1
     start = nparts_to_start_ind(Njobs_per_cpu)
-    return start, start + Njobs_per_cpu
+    return numpy.vstack([start, start + Njobs_per_cpu]).T
 
 
-def dump_particles(particles, clumps, clump_indxs, Njobs):
+def dump_particles(particles, particle_clumps, clumps, Nsplits, dumpfolder,
+                   Nsim, Nsnap, verbose=True):
     """
-    Save the data needed for each CPU so that it doesn't have to load
-    everything.
-    TODO: finish this stuff
+    Save the data needed for each split so that a process does not have to load
+    everything. These clumps should already be only the ones with particles.
 
-    Don't forget the halo index.
+    Parameters
+    ----------
+    particles : structured array
+        The particle array.
+    particle_clumps : 1-dimensional array
+        Array of particles' clump IDs.
+    clumps : structured array
+        The clumps array.
+    Nsplits : int
+        Number of times to split the clumps.
+    dumpfolder : str
+        Path to the folder where to dump the splits.
+    Nsim : int
+        CSiBORG simulation index.
+    Nsnap : int
+        Snapshot index.
+    verbose : bool, optional
+        Verbosity flag. By default `True`.
+
+    Returns
+    -------
+    None
     """
-    with_particles = clump_with_particles(clump_indxs, clumps["index"])
+    if particles.size != particle_clumps.size:
+        raise ValueError("`particles` must correspond to `particle_clumps`.")
+    # The starting clump index of each split
+    splits = distribute_halos(Nsplits, clumps)
+    fname = join(dumpfolder, "out_{}_snap_{}_{}.npz")
 
-    start, end = distribute_halos(Njobs, clump_indxs[with_particles])
+    iters = trange(Nsplits) if verbose else range(Nsplits)
+    tot = 0
+    for n in iters:
+        # Lower and upper array index of the clumps array
+        i, j = splits[n, :]
+        # Lower and upper clump index. Need - 1 not to take the last val.
+        ipart = clumps["index"][i]
+        jpart = clumps["index"][j - 1]
+        mask = (particle_clumps >= ipart) & (particle_clumps <= jpart)
+        # Dump it!
+        tot += mask.sum()
+        fout = fname.format(Nsim, Nsnap, n)
+        numpy.savez(fout, particles[mask], particle_clumps[mask], clumps[i:j])
 
-    for n in range(Njobs):
-        i = start[n]
-        j = end[n]
-
-        # The relevant clumps
-        clumps[i:j]
-        # Which particles belong to these clumps?
+    if tot != particle_clumps.size:
+        raise RuntimeError("Num. of dumped particles `{}` does not particle "
+                           "file size `{}`.".format(tot, particle_clumps.size))
