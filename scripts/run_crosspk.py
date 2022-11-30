678df3ecfef783952d61c668c8b2fa019b465b8d
rstiskalek <richard.stiskalek@protonmail.com>
2022-11-30 21:31:58 +0000

    edit docstring

--- scripts/run_crosspk.py
+++ scripts/run_crosspk.py
@@ -13,7 +13,8 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 """
-TODO: docs
+MPI script to calculate the matter cross power spectrum between CSiBORG
+IC realisations.
 """
 from argparse import ArgumentParser
 import numpy
