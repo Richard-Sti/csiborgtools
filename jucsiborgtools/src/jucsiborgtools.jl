e6b441b6d289ae7deed44fc7858d844705c3a67d
rstiskalek <richard.stiskalek@protonmail.com>
2022-12-12 14:52:33 +0000

    rename module

--- jucsiborgtools/src/jucsiborgtools.jl
+++ jucsiborgtools/src/jucsiborgtools.jl
@@ -13,10 +13,10 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-module JuliaCSiBORGTools
+module jucsiborgtools
 
 include("./particles_match.jl")
 
 export halo_parts
 
-end # module JuliaCSiBORGTools
+end
