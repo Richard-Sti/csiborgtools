ef4929c93ab4ae43a7861a4df53106c020be357a
rstiskalek <richard.stiskalek@protonmail.com>
2022-12-13 15:42:00 +0000

    Add particle dumping

--- scripts/run_dump_particles.py
+++ scripts/run_dump_particles.py
@@ -20,13 +20,14 @@ ID so that the index positions between the initial and final snapshots match.
 Don't forget to delete once used in Julia as these will take a lot of space
 (for all 100 simulations about 2TB), hence should be processed in smaller
 batches.
-
-TODO:
-- [ ] Check that the particle masses are equal when matched.
 """
 import numpy
 from datetime import datetime
-from os.path import join
+from mpi4py import MPI
+from os.path import join, isdir
+from os import mkdir
+from sys import stdout
+from gc import collect
 try:
     import csiborgtools
 except ModuleNotFoundError:
@@ -34,18 +35,30 @@ except ModuleNotFoundError:
     sys.path.append("../")
     import csiborgtools
 
+# Get MPI things
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+nproc = comm.Get_size()
 
 init_paths = csiborgtools.read.CSiBORGPaths(to_new=True)
 fin_paths = csiborgtools.read.CSiBORGPaths(to_new=False)
 nsims = init_paths.ic_ids
 
-dumpdir = "/mnt/extraspace/rstiskalek/csiborg/temp_initmatch_dump"
-fpart = join(dumpdir, "out_{}_{}_{}.npy")
-fclump = join(dumpdir, "out_{}_clumps.npy")
-pars = ["x", "y", "z", "M", "ID"]
+# Output files
+tempdumpdir = "/mnt/extraspace/rstiskalek/csiborg/temp_initmatch_dump"
+fpart = join(tempdumpdir, "out_{}_{}_{}.npy")
+fclump = join(tempdumpdir, "out_{}_clumps.npy")
 
-for nsim in nsims:
-    print("{}: saving simulation {}.".format(datetime.now(), nsim))
+permdumpdir = "/mnt/extraspace/rstiskalek/csiborg/temp_initmatch"
+
+for nsim in nsims[:1]:
+    if rank == 0:
+        print("{}: reading simulation {}.".format(datetime.now(), nsim))
+        stdout.flush()
+    # Check that the output folder for this sim exists
+    simdumpdir = join(permdumpdir, "out_{}".format(nsim))
+    if not isdir(simdumpdir):
+        mkdir(simdumpdir)
     # Set the snapshot numbers
     init_paths.set_info(nsim, init_paths.get_minimum_snapshot(nsim))
     fin_paths.set_info(nsim, fin_paths.get_maximum_snapshot(nsim))
@@ -53,37 +66,38 @@ for nsim in nsims:
     init_reader = csiborgtools.read.ParticleReader(init_paths)
     fin_reader = csiborgtools.read.ParticleReader(fin_paths)
 
-    # Read and sort the particle files by their particle IDs
-    particles = init_reader.read_particle(pars, verbose=False)
-    pid_order = numpy.argsort(particles["ID"])
-    particles = particles[pid_order]
-    # Put to a nicer array
-    out = numpy.full((particles.size, 4), numpy.nan, dtype=numpy.float32)
-    for i, par in enumerate(pars[:-1]):
-        out[:, i] = particles[par]
-    # Dump the particles and particle IDs
-    with open(fpart.format(nsim, "init", "part"), 'wb') as f:
-        numpy.save(f, out)
-    # Repeat for the particle IDs
-    with open(fpart.format(nsim, "init", "ID"), 'wb') as f:
-        numpy.save(f, particles["ID"])
+    # Read and sort the initial particle files by their particle IDs
+    part0 = init_reader.read_particle(["x", "y", "z", "M", "ID"],
+                                      verbose=False)
+    part0 = part0[numpy.argsort(part0["ID"])]
 
-    # Now repeat for the final snapshot but also get clump IDs
-    particles = fin_reader.read_particle(pars, verbose=False)
+    # Order the final snapshot clump IDs by the particle IDs
+    pid = fin_reader.read_particle(["ID"], verbose=False)["ID"]
     clump_ids = fin_reader.read_clumpid(verbose=False)
-    pid_order = numpy.argsort(particles["ID"])
-    particles = particles[pid_order]
-    clump_ids = clump_ids[pid_order]
-    # Put to a nicer array
-    out = numpy.full((particles.size, 4), numpy.nan, dtype=numpy.float32)
-    for i, par in enumerate(pars[:-1]):
-        out[:, i] = particles[par]
-    # Dump the particles and particle IDs
-    with open(fpart.format(nsim, "fin", "part"), 'wb') as f:
-        numpy.save(f, out)
-    # Repeat for the particle IDs
-    with open(fpart.format(nsim, "fin", "ID"), 'wb') as f:
-        numpy.save(f, particles["ID"])
-    # Clump IDs
-    with open(fclump.format(nsim), 'wb') as f:
-        numpy.save(f, clump_ids)
+    clump_ids = clump_ids[numpy.argsort(pid)]
+
+    del pid
+    collect()
+    # Get rid of the clumps whose index is 0 -- those are unassigned
+    mask = clump_ids > 0
+    clump_ids = clump_ids[mask]
+    part0 = part0[mask]
+    del mask
+    collect()
+
+    if rank == 0:
+        print("{}: dumping clumps for simulation.".format(datetime.now()))
+        stdout.flush()
+
+    # Grab unique clump IDs and loop over them
+    unique_clumpids = numpy.unique(clump_ids)
+
+    njobs = unique_clumpids.size
+    jobs = csiborgtools.fits.split_jobs(njobs, nproc)[rank]
+    for i in jobs:
+        n = unique_clumpids[i]
+        fout = join(simdumpdir, "clump_{}.npy".format(n))
+        print("Dumping clump {} to .. `{}`".format(n, fout))
+        stdout.flush()
+        with open(fout, "wb") as f:
+            numpy.save(f, part0[clump_ids == n])
