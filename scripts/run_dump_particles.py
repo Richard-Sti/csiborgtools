c4a7488ddf07c7396c6330b7ab671ac35161ad3f
rstiskalek <richard.stiskalek@protonmail.com>
2022-12-13 18:34:23 +0000

    add working script

--- scripts/run_dump_particles.py
+++ scripts/run_dump_particles.py
@@ -13,19 +13,20 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 """
-A script to dump particle positions at the final and initial snapshot in a
-format that is readable by Julia. Sorts the particle array by their particle
-ID so that the index positions between the initial and final snapshots match.
+A script to calculate the centre of mass of particles at redshift 70 that
+are grouped in a clump at present redshift.
 
-Don't forget to delete once used in Julia as these will take a lot of space
-(for all 100 simulations about 2TB), hence should be processed in smaller
-batches.
+Optionally also dumps the clumps information, however watch out as this will
+eat up a lot of memory.
 """
+from argparse import ArgumentParser
 import numpy
 from datetime import datetime
 from mpi4py import MPI
+from distutils.util import strtobool
 from os.path import join, isdir
 from os import mkdir
+from os import remove
 from sys import stdout
 from gc import collect
 try:
@@ -34,6 +35,11 @@ except ModuleNotFoundError:
     import sys
     sys.path.append("../")
     import csiborgtools
+
+parser = ArgumentParser()
+parser.add_argument("--dump_clumps", default=False,
+                    type=lambda x: bool(strtobool(x)))
+args = parser.parse_args()
 
 # Get MPI things
 comm = MPI.COMM_WORLD
@@ -45,19 +51,22 @@ fin_paths = csiborgtools.read.CSiBORGPaths(to_new=False)
 nsims = init_paths.ic_ids
 
 # Output files
-tempdumpdir = "/mnt/extraspace/rstiskalek/csiborg/temp_initmatch_dump"
-fpart = join(tempdumpdir, "out_{}_{}_{}.npy")
-fclump = join(tempdumpdir, "out_{}_clumps.npy")
-permdumpdir = "/mnt/extraspace/rstiskalek/csiborg/temp_initmatch"
+dumpdir = "/mnt/extraspace/rstiskalek/csiborg/initmatch"
+ftemp = join(dumpdir, "temp_{}_{}.npy")
+fperm = join(dumpdir, "clump_cm_{}.npy")
 
-for nsim in nsims[:1]:
+for nsim in nsims:
     if rank == 0:
         print("{}: reading simulation {}.".format(datetime.now(), nsim))
         stdout.flush()
     # Check that the output folder for this sim exists
-    simdumpdir = join(permdumpdir, "out_{}".format(nsim))
-    if not isdir(simdumpdir):
-        mkdir(simdumpdir)
+    clumpdumpdir = join(dumpdir, "out_{}".format(nsim))
+    if rank == 0 and not isdir(clumpdumpdir):
+        mkdir(clumpdumpdir)
+
+    # Barrier to make sure we created the directory with the rank 0
+    comm.Barrier()
+
     # Set the snapshot numbers
     init_paths.set_info(nsim, init_paths.get_minimum_snapshot(nsim))
     fin_paths.set_info(nsim, fin_paths.get_maximum_snapshot(nsim))
@@ -77,6 +86,7 @@ for nsim in nsims[:1]:
 
     del pid
     collect()
+
     # Get rid of the clumps whose index is 0 -- those are unassigned
     mask = clump_ids > 0
     clump_ids = clump_ids[mask]
@@ -95,8 +105,37 @@ for nsim in nsims[:1]:
     jobs = csiborgtools.fits.split_jobs(njobs, nproc)[rank]
     for i in jobs:
         n = unique_clumpids[i]
-        fout = join(simdumpdir, "clump_{}.npy".format(n))
-        print("Dumping clump {} to .. `{}`".format(n, fout))
+        x0 = part0[clump_ids == n]
+
+        # Center of mass
+        cm = numpy.asanyarray(
+            [numpy.average(x0[p], weights=x0["M"]) for p in ('x', 'y', 'z')])
+        # Dump the center of mass
+        with open(ftemp.format(nsim, n), 'wb') as f:
+            numpy.save(f, cm)
+        # Optionally dump the entire clump
+        if args.dump_clumps:
+            fout = join(clumpdumpdir, "clump_{}.npy".format(n))
+            stdout.flush()
+            with open(fout, "wb") as f:
+                numpy.save(f, part0[clump_ids == n])
+
+    comm.Barrier()
+    if rank == 0:
+        print("Collecting CM files...")
         stdout.flush()
-        with open(fout, "wb") as f:
-            numpy.save(f, part0[clump_ids == n])
+        # Collect the centre of masses and dump them
+        dtype = {"names": ['x', 'y', 'z'], "formats": [numpy.float32]*3}
+        out = numpy.full(njobs, numpy.nan, dtype=dtype)
+
+        for n in unique_clumpids:
+            with open(ftemp.format(nsim, n), 'rb') as f:
+                fin = numpy.load(f)
+            out['x'] = fin[0]
+            out['y'] = fin[1]
+            out['z'] = fin[2]
+            remove(ftemp.format(nsim, n))
+
+        print("Dumping CM files to .. `{}`.".format(fperm.format(n)))
+        with open(fperm.format(n), 'wb') as f:
+            numpy.save(f, out)
