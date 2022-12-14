b6653f9ae2d97d355b12ef4e7e6ee9a0dd1592ab
rstiskalek <richard.stiskalek@protonmail.com>
2022-12-14 07:40:38 +0000

    add initcm to halo cat

--- csiborgtools/read/make_cat.py
+++ csiborgtools/read/make_cat.py
@@ -15,13 +15,12 @@
 """
 Functions to read in the particle and clump files.
 """
-
 import numpy
 from os.path import join
 from tqdm import trange
 from copy import deepcopy
 from sklearn.neighbors import NearestNeighbors
-from .readsim import read_mmain
+from .readsim import (read_mmain, read_initcm)
 from ..utils import (flip_cols, add_columns)
 from ..units import (BoxUnits, cartesian_to_radec)
 
@@ -45,6 +44,7 @@ class HaloCatalogue:
     _data = None
     _knn = None
     _positions = None
+    _positions0 = None
 
     def __init__(self, paths, min_m500=None, max_dist=None):
         self._box = BoxUnits(paths)
@@ -142,6 +142,12 @@ class HaloCatalogue:
         # Cut on number of particles and finite m200
         data = data[(data["npart"] > 100) & numpy.isfinite(data["m200"])]
 
+        # Now also load the initial positions
+        initcm = read_initcm(self.n_sim, self.paths.initmatch_path)
+        if initcm is not None:
+            data = self.merge_initmatch_to_clumps(data, initcm)
+            flip_cols(data, "x0", "z0")
+
         # Calculate redshift
         pos = [data["peak_{}".format(p)] - 0.5 for p in ("x", "y", "z")]
         vel = [data["v{}".format(p)] for p in ("x", "y", "z")]
@@ -171,9 +177,14 @@ class HaloCatalogue:
         # Cut on separation
         data = data[data["dist"] < max_dist]
 
-        # Pre-allocate the positions array
+        # Pre-allocate the positions arrays
         self._positions = numpy.vstack(
             [data["peak_{}".format(p)] for p in ("x", "y", "z")]).T
+        # And do the unit transform
+        if initcm is not None:
+            data = self.box.convert_from_boxunits(data, ["x0", "y0", "z0"])
+            self._positions0 = numpy.vstack(
+                [data["{}0".format(p)] for p in ("x", "y", "z")]).T
 
         self._data = data
 
@@ -203,6 +214,35 @@ class HaloCatalogue:
         X[mask, 1] = mmain["sub_frac"]
         return add_columns(clumps, X, ["mass_mmain", "sub_frac"])
 
+    def merge_initmatch_to_clumps(self, clumps, initcat):
+        """
+        Merge columns from the `init_cm` files to the `clump` file.
+
+        Parameters
+        ----------
+        clumps : structured array
+            Clumps structured array.
+        initcat : structured array
+            Catalog with the clumps initial centre of mass at z = 70.
+
+        Returns
+        -------
+        out : structured array
+        """
+        # There are more initcat clumps, so check which ones have z = 0
+        # and then downsample
+        mask = numpy.isin(initcat["ID"], clumps["index"])
+        initcat = initcat[mask]
+        # Now the index ordering should match
+        if not numpy.alltrue(initcat["ID"] == clumps["index"]):
+            raise ValueError(
+                "Ordering of `initcat` and `clumps` is inconsistent.")
+
+        X = numpy.full((clumps.size, 3), numpy.nan)
+        for i, p in enumerate(['x', 'y', 'z']):
+            X[:, i] = initcat[p]
+        return add_columns(clumps, X, ["x0", "y0", "z0"])
+
     @property
     def positions(self):
         """
@@ -215,6 +255,21 @@ class HaloCatalogue:
             `x`, `y` and `z`.
         """
         return self._positions
+
+    @property
+    def positions0(self):
+        r"""
+        3D positions of halos at :math:`z = 70`.
+
+        Returns
+        -------
+        X : 2-dimensional array
+            Array of shape `(n_halos, 3)`, where the latter axis represents
+            `x`, `y` and `z`.
+        """
+        if self._positions0 is None:
+            raise RuntimeError("Initial positions are not set!")
+        return self._positions0
 
     @property
     def velocities(self):
@@ -272,6 +327,9 @@ class HaloCatalogue:
         return self.data.dtype.names
 
     def __getitem__(self, key):
+        initpars = ["x0", "y0", "z0"]
+        if key in initpars and key not in self.keys:
+            raise RuntimeError("Initial positions are not set!")
         return self._data[key]
 
 
