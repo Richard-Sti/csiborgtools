12dee4ac2d08439ad292ee67253b8835ac90c8ec
rstiskalek <richard.stiskalek@protonmail.com>
2023-03-17 16:58:34 +0000

    Simplify comments

--- scripts/run_singlematch.py
+++ scripts/run_singlematch.py
@@ -12,9 +12,7 @@
 # You should have received a copy of the GNU General Public License along
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-"""
-Script to test running the CSiBORG realisations matcher.
-"""
+"""A script to calculate overlap between two CSiBORG realisations."""
 import numpy
 from argparse import ArgumentParser
 from distutils.util import strtobool
