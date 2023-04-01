b7eb2bc779b70b4f948e2b832bea3f083ca5f0a5
rstiskalek <richard.stiskalek@protonmail.com>
2023-04-01 09:53:03 +0100

    change sub script

--- scripts/run_knn.sh
+++ scripts/run_knn.sh
@@ -1,5 +1,5 @@
-nthreads=30
-memory=7
+nthreads=102
+memory=4
 queue="berg"
 env="/mnt/zfsusers/rstiskalek/csiborgtools/venv_galomatch/bin/python"
 file="run_knn.py"
@@ -7,13 +7,9 @@ file="run_knn.py"
 rmin=0.01
 rmax=100
 nneighbours=16
-nsamples=1000000000
-batch_size=10000000
+nsamples=100000000
+batch_size=1000000
 neval=10000
-
-# 1000,000,0
-# 10000000  # 1e7
-# 1000000000
 
 pythoncm="$env $file --rmin $rmin --rmax $rmax --nneighbours $nneighbours --nsamples $nsamples --neval $neval"
 
