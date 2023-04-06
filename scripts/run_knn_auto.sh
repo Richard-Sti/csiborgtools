9c7d2beafb1f2ca86b360a19c996f727a02f814c
rstiskalek <richard.stiskalek@protonmail.com>
2023-04-07 00:49:39 +0100

    Add seperate autoknn script & config file

---
+++ scripts/run_knn_auto.sh
@@ -0,0 +1,19 @@
+nthreads=151
+memory=4
+queue="cmb"
+env="/mnt/zfsusers/rstiskalek/csiborgtools/venv_galomatch/bin/python"
+file="run_knn_auto.py"
+
+
+
+# pythoncm="$env $file --runs 001 002 003 004 005 006 007 008 009 010"
+pythoncm="$env $file --runs 001 002 003"
+
+echo $pythoncm
+$pythoncm
+
+# cm="addqueue -q $queue -n $nthreads -m $memory $pythoncm"
+# echo "Submitting:"
+# echo $cm
+# echo
+# $cm
