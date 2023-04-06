478d8151f003ecf1a3ac0b8d8bc5513c29162ee0
rstiskalek <richard.stiskalek@protonmail.com>
2023-04-07 00:50:44 +0100

    Edit submission script

--- scripts/run_knn_auto.sh
+++ scripts/run_knn_auto.sh
@@ -1,4 +1,4 @@
-nthreads=151
+nthreads=101
 memory=4
 queue="cmb"
 env="/mnt/zfsusers/rstiskalek/csiborgtools/venv_galomatch/bin/python"
@@ -6,14 +6,13 @@ file="run_knn_auto.py"
 
 
 
-# pythoncm="$env $file --runs 001 002 003 004 005 006 007 008 009 010"
-pythoncm="$env $file --runs 001 002 003"
+pythoncm="$env $file --runs 001 002 003 004 005 006 007 008 009 010"
 
-echo $pythoncm
-$pythoncm
+# echo $pythoncm
+# $pythoncm
 
-# cm="addqueue -q $queue -n $nthreads -m $memory $pythoncm"
-# echo "Submitting:"
-# echo $cm
-# echo
-# $cm
+cm="addqueue -q $queue -n $nthreads -m $memory $pythoncm"
+echo "Submitting:"
+echo $cm
+echo
+$cm
