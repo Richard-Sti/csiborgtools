665090349d55792b41af2d6b96e5fcedac3a7e3b
rstiskalek <richard.stiskalek@protonmail.com>
2023-04-08 12:18:44 +0100

    edit submit

--- scripts/knn_auto.sh
+++ scripts/knn_auto.sh
@@ -4,17 +4,15 @@ queue="cmb"
 env="/mnt/zfsusers/rstiskalek/csiborgtools/venv_galomatch/bin/python"
 file="knn_auto.py"
 
-
-runs="mass001 mass001_perm mass002 mass002_perm mass003 mass003_perm mass001_spinlow mass001_spinlow_perm mass001_spinhigh mass001_spinhigh_perm mass002_spinlow mass002_spinlow_perm mass002_spinhigh mass002_spinhigh_perm mass003_spinlow mass003_spinlow_perm mass_003_spinhigh mass_003_spinhigh_perm"
-
+runs="mass001 mass001_spinlow mass001_spinhigh mass001_spinmedian_perm mass001_spinmedian_cross"
 
 pythoncm="$env $file --runs $runs"
 
-# echo $pythoncm
-# $pythoncm
+echo $pythoncm
+$pythoncm
 
-cm="addqueue -q $queue -n $nthreads -m $memory $pythoncm"
-echo "Submitting:"
-echo $cm
-echo
-$cm
+# cm="addqueue -q $queue -n $nthreads -m $memory $pythoncm"
+# echo "Submitting:"
+# echo $cm
+# echo
+# $cm
