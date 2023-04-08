d3a1682e7d770508546c0647abbef4cdb809dfe8
rstiskalek <richard.stiskalek@protonmail.com>
2023-04-08 22:57:40 +0100

    Split

--- csiborgtools/clustering/2pcf.py
+++ csiborgtools/clustering/2pcf.py
@@ -20,55 +20,7 @@ NOTE: This is an old script that needs to be updated.
 import numpy
 from Corrfunc.mocks import DDtheta_mocks
 from Corrfunc.utils import convert_3d_counts_to_cf
-from warnings import warn
-
-
-def get_randoms_sphere(N, seed=42):
-    """
-    Generate random points on a sphere.
-
-    Parameters
-    ----------
-    N : int
-        Number of points.
-    seed : int
-        Random seed.
-
-    Returns
-    -------
-    ra : 1-dimensional array
-        Right ascension in :math:`[0, 360)` degrees.
-    dec : 1-dimensional array
-        Declination in :math:`[-90, 90]` degrees.
-    """
-    gen = numpy.random.default_rng(seed)
-    ra = gen.random(N) * 360
-    dec = numpy.rad2deg(numpy.arcsin(2 * (gen.random(N) - 0.5)))
-    return ra, dec
-
-
-def wrapRA(ra, degrees=True):
-    """
-    Wrap the right ascension from :math:`[-180, 180)` to :math`[0, 360)`
-    degrees or equivalently if `degrees=False` in radians.
-
-    Paramaters
-    ----------
-    ra : 1-dimensional array
-        Right ascension values.
-    degrees : float, optional
-        Whether the right ascension is in degrees.
-
-    Returns
-    -------
-    ra : 1-dimensional array
-        Wrapped around right ascension.
-    """
-    mask = ra < 0
-    if numpy.sum(mask) == 0:
-        warn("No negative right ascension found.")
-    ra[mask] += 360 if degrees else 2 * numpy.pi
-    return ra
+from .utils import (rvs_on_sphere, wrapRA)
 
 
 def sphere_angular_tpcf(bins, RA1, DEC1, RA2=None, DEC2=None, nthreads=1,
@@ -118,11 +70,11 @@ def sphere_angular_tpcf(bins, RA1, DEC1, RA2=None, DEC2=None, nthreads=1,
     NR1 = ND1 * Nmult
     NR2 = ND2 * Nmult
     # Generate randoms. Note that these are over the sphere!
-    randRA1, randDEC1 = get_randoms_sphere(NR1, seed1)
-    randRA2, randDEC2 = get_randoms_sphere(NR2, seed2)
+    randRA1, randDEC1 = rvs_on_sphere(NR1, indeg=True, random_state=seed1)
+    randRA2, randDEC2 = rvs_on_sphere(NR2, indeg=True, random_state=seed2)
     # Wrap RA
-    RA1 = wrapRA(numpy.copy(RA1))
-    RA2 = wrapRA(numpy.copy(RA2))
+    RA1 = wrapRA(numpy.copy(RA1), indeg=True)
+    RA2 = wrapRA(numpy.copy(RA2), indeg=True)
     # Calculate pairs
     D1D2 = DDtheta_mocks(0, nthreads, bins, RA1, DEC1, RA2=RA2, DEC2=DEC2)
     D1R2 = DDtheta_mocks(0, nthreads, bins, RA1, DEC1,
--- csiborgtools/clustering/utils.py
+++ csiborgtools/clustering/utils.py
@@ -13,13 +13,19 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 """Clustering support functions."""
+from warnings import warn
 import numpy
+
+
+###############################################################################
+#                            Random points                                    #
+###############################################################################
 
 
 def rvs_in_sphere(nsamples, R, random_state=42, dtype=numpy.float32):
     """
-    Generate random samples in a sphere of radius `R` centered at the
-    origin.
+    Generate random uniform samples in a sphere of radius `R` in Cartesian
+    coordinates.
 
     Parameters
     ----------
@@ -37,13 +43,69 @@ def rvs_in_sphere(nsamples, R, random_state=42, dtype=numpy.float32):
     samples : 2-dimensional array of shape `(nsamples, 3)`
     """
     gen = numpy.random.default_rng(random_state)
-    # Sample spherical coordinates
-    r = gen.uniform(0, 1, nsamples).astype(dtype)**(1/3) * R
-    theta = 2 * numpy.arcsin(gen.uniform(0, 1, nsamples).astype(dtype))
-    phi = 2 * numpy.pi * gen.uniform(0, 1, nsamples).astype(dtype)
-    # Convert to cartesian coordinates
+    # Spherical
+    r = gen.random(nsamples, dtype=dtype)**(1/3) * R
+    theta = 2 * numpy.arcsin(gen.random(nsamples, dtype=dtype))
+    phi = 2 * numpy.pi * gen.random(nsamples, dtype=dtype)
+    # Cartesian
     x = r * numpy.sin(theta) * numpy.cos(phi)
     y = r * numpy.sin(theta) * numpy.sin(phi)
     z = r * numpy.cos(theta)
 
     return numpy.vstack([x, y, z]).T
+
+
+def rvs_on_sphere(nsamples, indeg, random_state=42, dtype=numpy.float32):
+    r"""
+    Generate random uniform samples on the surface of a sphere.
+
+    Parameters
+    ----------
+    nsamples : int
+        Number of samples to generate.
+    indeg : bool, optional
+        Whether to return the right ascension and declination in degrees.
+    random_state : int
+        Random state for the random number generator.
+    dtype : numpy dtype, optional
+        Data type, by default `numpy.float32`.
+
+    Returns
+    -------
+    out : 2-dimensional array of shape `(nsamples, 2)`
+        RA in :math:`[0, 2\pi)` and dec in :math:`[-\pi / 2, \pi / 2]`,
+        respectively. If `indeg` then converted to degrees.
+    """
+    gen = numpy.random.default_rng(random_state)
+    ra = 2 * numpy.pi * gen.random(nsamples, dtype=dtype)
+    dec = numpy.arcsin(2 * (gen.random(nsamples, dtype=dtype) - 0.5))
+    if indeg:
+        ra = numpy.deg2rad(ra)
+        dec = numpy.deg2rad(dec)
+    return numpy.vstack([ra, dec]).T
+
+###############################################################################
+#                               RA wrapping                                   #
+###############################################################################
+
+def wrapRA(ra, indeg):
+    """
+    Wrap RA from :math:`[-180, 180)` to :math`[0, 360)` degrees if `indeg` or
+    equivalently in radians otherwise.
+
+    Paramaters
+    ----------
+    ra : 1-dimensional array
+        Right ascension.
+    indeg : bool
+        Whether the right ascension is in degrees.
+
+    Returns
+    -------
+    wrapped_ra : 1-dimensional array
+    """
+    mask = ra < 0
+    if numpy.sum(mask) == 0:
+        warn("No negative right ascension found.", UserWarning())
+    ra[mask] += 360 if indeg else 2 * numpy.pi
+    return ra
