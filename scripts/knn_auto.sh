17c0ca8e936a49e17320f19e949e274cec17ea66
rstiskalek <richard.stiskalek@protonmail.com>
2023-04-08 22:32:02 +0100

    Edit scripts

--- scripts/knn_auto.sh
+++ scripts/knn_auto.sh
@@ -1,18 +1,21 @@
-nthreads=50
+nthreads=102
 memory=4
-queue="cmb"
+queue="berg"
 env="/mnt/zfsusers/rstiskalek/csiborgtools/venv_galomatch/bin/python"
 file="knn_auto.py"
 
-runs="mass001 mass001_spinlow mass001_spinhigh mass001_spinmedian_perm mass001_spinmedian_cross"
+#runs="mass001 mass001_spinlow mass001_spinhigh mass001_spinmedian_perm mass001_spinmedian_cross"
+# runs="mass001_spinmedian_cross_perm"
+
+runs="mass001_spinlow_cross_perm mass001_spinhigh_cross_perm"
 
 pythoncm="$env $file --runs $runs"
 
-echo $pythoncm
-$pythoncm
+# echo $pythoncm
+# $pythoncm
 
-# cm="addqueue -q $queue -n $nthreads -m $memory $pythoncm"
-# echo "Submitting:"
-# echo $cm
-# echo
-# $cm
+cm="addqueue -q $queue -n $nthreads -m $memory $pythoncm"
+echo "Submitting:"
+echo $cm
+echo
+$cm
