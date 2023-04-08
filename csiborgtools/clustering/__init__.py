aea731ab824b8b94407a8681e82d09b9e83aa088
rstiskalek <richard.stiskalek@protonmail.com>
2023-04-08 22:28:53 +0100

    Add init

--- csiborgtools/clustering/__init__.py
+++ csiborgtools/clustering/__init__.py
@@ -12,3 +12,6 @@
 # You should have received a copy of the GNU General Public License along
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+from .knn import kNN_CDF
+# from .correlation import (get_randoms_sphere, sphere_angular_tpcf) # noqa
