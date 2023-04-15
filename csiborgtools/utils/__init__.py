4a8694a5cee9c5fb1821911f781a18e6a850bcbb
rstiskalek <richard.stiskalek@protonmail.com>
2023-04-15 20:47:43 +0100

    Add import

--- csiborgtools/utils/__init__.py
+++ csiborgtools/utils/__init__.py
@@ -12,7 +12,7 @@
 # You should have received a copy of the GNU General Public License along
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-
+import numpy
 from datetime import datetime
 from .recarray_manip import (cols_to_structured, add_columns, rm_columns,  # noqa
                              list_to_ndarray, array_to_structured,  # noqa
