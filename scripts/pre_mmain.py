1f0dfbacf7934046bbe248f087f4f1acd22b0f36
rstiskalek <richard.stiskalek@protonmail.com>
2023-04-15 17:29:07 +0100

    Fix list requirement

--- scripts/pre_mmain.py
+++ scripts/pre_mmain.py
@@ -47,7 +47,7 @@ def do_mmain(nsim):
 
 if nproc > 1:
     if rank == 0:
-        tasks = paths.get_ics(tonew=False)
+        tasks = list(paths.get_ics(tonew=False))
         master_process(tasks, comm, verbose=True)
     else:
         worker_process(do_mmain, comm, verbose=False)
