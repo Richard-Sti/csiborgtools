2b5dd73b4d48eff7f8cabdbbffff4c064b24e662
rstiskalek <richard.stiskalek@protonmail.com>
2023-04-18 00:47:01 +0100

    add checks

--- csiborgtools/fits/__init__.py
+++ csiborgtools/fits/__init__.py
@@ -12,5 +12,5 @@
 # You should have received a copy of the GNU General Public License along
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-from .halo import Clump  # noqa
+from .halo import Clump, Halo  # noqa
 from .haloprofile import NFWPosterior, NFWProfile  # noqa
