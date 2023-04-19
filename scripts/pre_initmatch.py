074f79e62eedd47d03f946a66eba61b498c4a60e
rstiskalek <richard.stiskalek@protonmail.com>
2023-04-19 15:59:28 +0100

    pep8

--- scripts/pre_initmatch.py
+++ scripts/pre_initmatch.py
@@ -13,6 +13,7 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 """
+TODO: these docs
 A script to calculate the centre of mass of particles at redshift 70 that
 are grouped in a clump at present redshift.
 
@@ -33,8 +34,10 @@ try:
     import csiborgtools
 except ModuleNotFoundError:
     import sys
+
     sys.path.append("../")
     import csiborgtools
+
 
 # Get MPI things
 comm = MPI.COMM_WORLD
@@ -43,30 +46,28 @@ nproc = comm.Get_size()
 
 # Argument parser
 parser = ArgumentParser()
-parser.add_argument("--dump_clumps", type=lambda x: bool(strtobool(x)))
+parser.add_argument("--dump", type=lambda x: bool(strtobool(x)))
 args = parser.parse_args()
-
 paths = csiborgtools.read.CSiBORGPaths(**csiborgtools.paths_glamdring)
-nsims = paths.get_ics(tonew=True)
-
 # Temporary output file
 ftemp = join(paths.dumpdir, "temp", "initmatch_{}_{}_{}.npy")
 
-for nsim in nsims:
+# We loop over all particles and then use MPI when matching halos to the
+# initial snapshot and dumping them.
+for nsim in paths.get_ics(tonew=True):
     if rank == 0:
-        print("{}: reading simulation {}.".format(datetime.now(), nsim),
-              flush=True)
-    nsnap_max = max(paths.get_snapshots(nsim))
+        print("{}: reading simulation {}.".format(datetime.now(), nsim), flush=True)
+    nsnap = max(paths.get_snapshots(nsim))
     reader = csiborgtools.read.ParticleReader(paths)
 
-    # Read and sort the initial particle files by their particle IDs
-    part0 = reader.read_particle(1, nsim, ["x", "y", "z", "M", "ID"],
-                                 verbose=False)
+    # We first read all particles in the initial snap shot and sort them
+    # according to their particle ID.
+    part0 = reader.read_particle(1, nsim, ["x", "y", "z", "M", "ID"], verbose=False)
     part0 = part0[numpy.argsort(part0["ID"])]
 
     # Order the final snapshot clump IDs by the particle IDs
-    pid = reader.read_particle(nsnap_max, nsim, ["ID"], verbose=False)["ID"]
-    clump_ids = reader.read_clumpid(nsnap_max, nsim, verbose=False)
+    pid = reader.read_particle(nsnap, nsim, ["ID"], verbose=False)["ID"]
+    clump_ids = reader.read_clumpid(nsnap, nsim, verbose=False)
     clump_ids = clump_ids[numpy.argsort(pid)]
 
     del pid
@@ -80,8 +81,7 @@ for nsim in nsims:
     collect()
 
     if rank == 0:
-        print("{}: dumping intermediate files.".format(datetime.now()),
-              flush=True)
+        print("{}: dumping intermediate files.".format(datetime.now()), flush=True)
 
     # Grab unique clump IDs and loop over them
     unique_clumpids = numpy.unique(clump_ids)
@@ -97,10 +97,10 @@ for nsim in nsims:
         patch = csiborgtools.match.dist_percentile(dist, [99], distmax=0.075)
 
         # Dump the center of mass
-        with open(ftemp.format(nsim, n, "cm"), 'wb') as f:
+        with open(ftemp.format(nsim, n, "cm"), "wb") as f:
             numpy.save(f, cm)
         # Dump the Lagrangian patch size
-        with open(ftemp.format(nsim, n, "lagpatch"), 'wb') as f:
+        with open(ftemp.format(nsim, n, "lagpatch"), "wb") as f:
             numpy.save(f, patch)
         # Dump the entire clump
         if args.dump_clumps:
@@ -112,11 +112,12 @@ for nsim in nsims:
 
     comm.Barrier()
     if rank == 0:
-        print("{}: collecting summary files...".format(datetime.now()),
-              flush=True)
+        print("{}: collecting summary files...".format(datetime.now()), flush=True)
         # Collect the centre of masses, patch size, etc. and dump them
-        dtype = {"names": ['x', 'y', 'z', "lagpatch", "ID"],
-                 "formats": [numpy.float32] * 4 + [numpy.int32]}
+        dtype = {
+            "names": ["x", "y", "z", "lagpatch", "ID"],
+            "formats": [numpy.float32] * 4 + [numpy.int32],
+        }
         out = numpy.full(njobs, numpy.nan, dtype=dtype)
 
         for i, n in enumerate(unique_clumpids):
@@ -124,9 +125,9 @@ for nsim in nsims:
             fpath = ftemp.format(nsim, n, "cm")
             with open(fpath, "rb") as f:
                 fin = numpy.load(f)
-                out['x'][i] = fin[0]
-                out['y'][i] = fin[1]
-                out['z'][i] = fin[2]
+                out["x"][i] = fin[0]
+                out["y"][i] = fin[1]
+                out["z"][i] = fin[2]
             remove(fpath)
 
             # Load in the patch size
@@ -138,29 +139,30 @@ for nsim in nsims:
             # Store the halo ID
             out["ID"][i] = n
 
-        print("{}: dumping to .. `{}`.".format(
-            datetime.now(), paths.initmatch_path(nsim, "cm")), flush=True)
-        with open(paths.initmatch_path(nsim, "cm"), 'wb') as f:
+        print(
+            "{}: dumping to .. `{}`.".format(
+                datetime.now(), paths.initmatch_path(nsim, "cm")
+            ),
+            flush=True,
+        )
+        with open(paths.initmatch_path(nsim, "cm"), "wb") as f:
             numpy.save(f, out)
 
         if args.dump_clumps:
-            print("{}: collecting particle files...".format(datetime.now()),
-                  flush=True)
+            print("{}: collecting particle files...".format(datetime.now()), flush=True)
             out = [None] * unique_clumpids.size
-            dtype = {"names": ["clump", "ID"],
-                     "formats": [object, numpy.int32]}
+            dtype = {"names": ["clump", "ID"], "formats": [object, numpy.int32]}
             out = numpy.full(unique_clumpids.size, numpy.nan, dtype=dtype)
             for i, n in enumerate(unique_clumpids):
                 fpath = ftemp.format(nsim, n, "clump")
-                with open(fpath, 'rb') as f:
+                with open(fpath, "rb") as f:
                     fin = numpy.load(f)
                 out["clump"][i] = fin
                 out["ID"][i] = n
                 remove(fpath)
 
             fout = paths.initmatch_path(nsim, "particles")
-            print("{}: dumping to .. `{}`.".format(datetime.now(), fout),
-                  flush=True)
+            print("{}: dumping to .. `{}`.".format(datetime.now(), fout), flush=True)
             with open(fout, "wb") as f:
                 numpy.save(f, out)
 
