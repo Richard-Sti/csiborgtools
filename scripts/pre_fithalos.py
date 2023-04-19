758ca38679cc3cfc2c67b80dd4efad9a1b7f8276
rstiskalek <richard.stiskalek@protonmail.com>
2023-04-19 09:40:14 +0100

    add parents support

--- scripts/pre_fithalos.py
+++ scripts/pre_fithalos.py
@@ -47,6 +47,7 @@ partreader = csiborgtools.read.ParticleReader(paths)
 nfwpost = csiborgtools.fits.NFWPosterior()
 ftemp = join(paths.temp_dumpdir, "fit_clump_{}_{}_{}.npy")
 cols_collect = [
+    ("index", numpy.int32),
     ("npart", numpy.int32),
     ("totpartmass", numpy.float32),
     ("vx", numpy.float32),
@@ -64,10 +65,14 @@ cols_collect = [
 ]
 
 
-def fit_clump(particles, clump, box):
-    obj = csiborgtools.fits.Clump(particles, clump, box)
+def fit_clump(particles, clump_info, box):
+    """
+    Fit an object. Can be eithe a clump or a parent halo.
+    """
+    obj = csiborgtools.fits.Clump(particles, clump_info, box)
 
     out = {}
+    out["index"] = clump_info["index"]
     out["npart"] = len(obj)
     out["totpartmass"] = numpy.sum(obj["M"])
     out["vx"] = numpy.average(obj.vel[:, 0], weights=obj["M"])
@@ -86,6 +91,37 @@ def fit_clump(particles, clump, box):
     return out
 
 
+def load_clump_particles(clumpid, particle_archive, clumps_cat):
+    """
+    Load a clump's particles from the particle archive. If it is not there, i.e
+    clump has no associated particles, return `None`.
+    """
+    try:
+        part = particle_archive[str(clumpid)]
+    except KeyError:
+        part = None
+    return part
+
+
+def load_parent_particles(clumpid, particle_archive, clumps_cat):
+    """
+    Load a parent halo's particles.
+    """
+    indxs = clumps_cat["index"][clumps_cat["parent"] == clumpid]
+
+    # We first load the particles of each clump belonging to this parent and then
+    # concatenate them for further analysis.
+    clumps = []
+    for ind in indxs:
+        parts = load_clump_particles(ind, particle_archive, clumps_cat)
+        if parts is not None:
+            clumps.append([parts, None])
+
+    if len(clumps) == 0:
+        return None
+    return csiborgtools.match.concatenate_clumps(clumps, include_velocities=True)
+
+
 for i, nsim in enumerate(paths.get_ics(tonew=False)):
     if rank == 0:
         print(
@@ -97,24 +133,34 @@ for i, nsim in enumerate(paths.get_ics(tonew=False)):
 
     # Archive of clumps, keywords are their clump IDs
     particle_archive = numpy.load(paths.split_path(nsnap, nsim))
-    clumpsarr = partreader.read_clumps(nsnap, nsim, cols=["index", "x", "y", "z"])
-    nclumps = clumpsarr.size
-
+    clumps_cat = csiborgtools.read.ClumpsCatalogue(
+        nsim, paths, maxdist=None, minmass=None
+    )
+    # We check whether we fit halos or clumps, will be indexing over different
+    # iterators.
+    if args.kind == "halos":
+        ismain = clumps_cat.ismain
+    else:
+        ismain = numpy.ones(len(clumps_cat), dtype=bool)
+    ntasks = len(clumps_cat)
     # We split the clumps among the processes. Each CPU calculates a fraction
-    # of them and dumps the results in a structured array.
-    jobs = csiborgtools.fits.split_jobs(nclumps, nproc)
+    # of them and dumps the results in a structured array. Even if we are
+    # calculating parent halo this index runs over all clumps.
+    jobs = csiborgtools.fits.split_jobs(ntasks, nproc)
     out = csiborgtools.read.cols_to_structured(len(jobs[rank]), cols_collect)
     for i in tqdm(jobs[rank]) if nproc == 1 else jobs[rank]:
-        clumpid = clumpsarr["index"][i]
-        # We check whether this clump has some associated particles and then
-        # fit it and store the results in `out`.
-        try:
-            part = particle_archive[str(clumpid)]
-        except KeyError:
-            part = None
+        # If we are fitting halos and this clump is not a main, then continue.
+        if args.kind == "halos" and not ismain[i]:
+            continue
+
+        clumpid = clumps_cat["index"][i]
+        if args.kind == "halos":
+            part = load_parent_particles(clumpid, particle_archive, clumps_cat)
+        else:
+            part = load_clump_particles(clumpid, particle_archive, clumps_cat)
 
         if part is not None:
-            _out = fit_clump(part, clumpsarr[i], box)
+            _out = fit_clump(part, clumps_cat[i], box)
 
         for key in _out.keys():
             out[key][i] = _out[key]
@@ -133,7 +179,7 @@ for i, nsim in enumerate(paths.get_ics(tonew=False)):
         )
         # We write to the output array. Load data from each CPU and append to
         # the output array.
-        out = csiborgtools.read.cols_to_structured(nclumps, cols_collect)
+        out = csiborgtools.read.cols_to_structured(ntasks, cols_collect)
         k = 0
         for i in range(nproc):
             inp = numpy.load(ftemp.format(str(nsim).zfill(5), str(nsnap).zfill(5), i))
@@ -141,6 +187,11 @@ for i, nsim in enumerate(paths.get_ics(tonew=False)):
                 for key in inp.dtype.names:
                     out[key][k] = inp[key][j]
                 k += 1
+
+        # If we were analysing main halos, then remove array indices that do
+        # not correspond to parent halos.
+        if args.kind == "halos":
+            out = out[ismain]
 
         fout = paths.structfit_path(nsnap, nsim, "clumps")
         print("Saving to `{}`.".format(fout), flush=True)
