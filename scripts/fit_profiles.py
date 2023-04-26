d63fc51630ba7f273da0b930ddac94b35c918a03
rstiskalek <richard.stiskalek@protonmail.com>
2023-04-26 23:58:41 +0100

    Update TODO

--- scripts/fit_profiles.py
+++ scripts/fit_profiles.py
@@ -12,7 +12,10 @@
 # You should have received a copy of the GNU General Public License along
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-"""A script to calculate the particle's separation from the CM and save it."""
+"""
+A script to calculate the particle's separation from the CM and save it.
+Currently MPI is not supported.
+"""
 from argparse import ArgumentParser
 from datetime import datetime
 from gc import collect
