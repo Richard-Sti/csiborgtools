d24373c7920c86d890e96a250e8349faed03aa92
rstiskalek <richard.stiskalek@protonmail.com>
2023-05-12 15:51:47 +0100

    Basic renaming

--- csiborgtools/read/__init__.py
+++ csiborgtools/read/__init__.py
@@ -12,7 +12,7 @@
 # You should have received a copy of the GNU General Public License along
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-from .box_units import BoxUnits  # noqa
+from .box_units import CSiBORGBox  # noqa
 from .halo_cat import ClumpsCatalogue, HaloCatalogue  # noqa
 from .knn_summary import kNNCDFReader  # noqa
 from .obs import (SDSS, MCXCClusters, PlanckClusters, TwoMPPGalaxies,  # noqa
