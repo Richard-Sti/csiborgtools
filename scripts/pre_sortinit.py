b26953b26609fca142fa57b5fbcfc86492271d89
rstiskalek <richard.stiskalek@protonmail.com>
2023-07-28 18:43:43 +0100

    Edit the docstring

--- scripts/pre_sortinit.py
+++ scripts/pre_sortinit.py
@@ -12,7 +12,7 @@
 # You should have received a copy of the GNU General Public License along
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-"""
+r"""
 Script to sort the initial snapshot particles according to their final
 snapshot ordering, which is sorted by the halo IDs.
 
