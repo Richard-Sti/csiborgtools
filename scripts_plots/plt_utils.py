41c284e4b15fe44fe1107523a9bd725d419030bb
rstiskalek <richard.stiskalek@protonmail.com>
2023-08-15 12:14:07 +0100

    Add weighted stats

--- scripts_plots/plt_utils.py
+++ scripts_plots/plt_utils.py
@@ -57,6 +57,49 @@ def latex_float(*floats, n=2):
     return latex_floats
 
 
+def nan_weighted_average(arr, weights=None, axis=None):
+    if weights is None:
+        weights = numpy.ones_like(arr)
+
+    valid_entries = ~numpy.isnan(arr)
+
+    # Set NaN entries in arr to 0 for computation
+    arr = numpy.where(valid_entries, arr, 0)
+
+    # Set weights of NaN entries to 0
+    weights = numpy.where(valid_entries, weights, 0)
+
+    # Compute the weighted sum and the sum of weights along the axis
+    weighted_sum = numpy.sum(arr * weights, axis=axis)
+    sum_weights = numpy.sum(weights, axis=axis)
+
+    return weighted_sum / sum_weights
+
+
+def nan_weighted_std(arr, weights=None, axis=None, ddof=0):
+    if weights is None:
+        weights = numpy.ones_like(arr)
+
+    valid_entries = ~numpy.isnan(arr)
+
+    # Set NaN entries in arr to 0 for computation
+    arr = numpy.where(valid_entries, arr, 0)
+
+    # Set weights of NaN entries to 0
+    weights = numpy.where(valid_entries, weights, 0)
+
+    # Calculate weighted mean
+    weighted_mean = numpy.sum(
+        arr * weights, axis=axis) / numpy.sum(weights, axis=axis)
+
+    # Calculate the weighted variance
+    variance = numpy.sum(
+        weights * (arr - numpy.expand_dims(weighted_mean, axis))**2, axis=axis)
+    variance /= numpy.sum(weights, axis=axis) - ddof
+
+    return numpy.sqrt(variance)
+
+
 def compute_error_bars(x, y, xbins, sigma):
     bin_indices = numpy.digitize(x, xbins)
     y_medians = numpy.array([numpy.median(y[bin_indices == i])
@@ -65,10 +108,15 @@ def compute_error_bars(x, y, xbins, sigma):
     lower_pct = 100 * 0.5 * (1 - erf(sigma / numpy.sqrt(2)))
     upper_pct = 100 - lower_pct
 
-    y_lower = [numpy.percentile(y[bin_indices == i], lower_pct)
-               for i in range(1, len(xbins))]
-    y_upper = [numpy.percentile(y[bin_indices == i], upper_pct)
-               for i in range(1, len(xbins))]
+    y_lower = numpy.full(len(y_medians), numpy.nan)
+    y_upper = numpy.full(len(y_medians), numpy.nan)
+
+    for i in range(len(y_medians)):
+        if numpy.sum(bin_indices == i + 1) == 0:
+            continue
+
+        y_lower[i] = numpy.percentile(y[bin_indices == i + 1], lower_pct)
+        y_upper[i] = numpy.percentile(y[bin_indices == i + 1], upper_pct)
 
     yerr = (y_medians - numpy.array(y_lower), numpy.array(y_upper) - y_medians)
 
