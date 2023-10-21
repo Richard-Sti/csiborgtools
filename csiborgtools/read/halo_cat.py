b6aacca7cd09fd3d4d649129104bdca5af059137
rstiskalek <richard.stiskalek@protonmail.com>
2023-10-21 14:00:42 +0100

    Docs

--- csiborgtools/read/halo_cat.py
+++ csiborgtools/read/halo_cat.py
@@ -12,11 +12,7 @@
 # You should have received a copy of the GNU General Public License along
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-"""
-Simulation catalogues:
-    - CSiBORG: FoF halo catalogue.
-    - Quijote: FoF halo catalogue.
-"""
+"""Simulation catalogues."""
 from abc import ABC
 from collections import OrderedDict
 from functools import lru_cache
