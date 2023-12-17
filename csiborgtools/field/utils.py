b5404d572cdcbea9c46e147034950003833555b4
rstiskalek <richard.stiskalek@protonmail.com>
2023-12-17 11:31:29 +0000

    Bring back utils

---
+++ csiborgtools/field/utils.py
@@ -0,0 +1,44 @@
+# Copyright (C) 2022 Richard Stiskalek
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 3 of the License, or (at your
+# option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+"""
+Utility functions used in the rest of the `density` module to avoid circular
+imports.
+"""
+from numba import jit
+import numpy
+
+
+def force_single_precision(x):
+    """
+    Attempt to convert an array `x` to float 32.
+    """
+    if x.dtype != numpy.float32:
+        x = x.astype(numpy.float32)
+    return x
+
+
+@jit(nopython=True)
+def divide_nonzero(field0, field1):
+    """
+    Perform in-place `field0 /= field1` but only where `field1 != 0`.
+    """
+    assert field0.shape == field1.shape, "Field shapes must match."
+
+    imax, jmax, kmax = field0.shape
+    for i in range(imax):
+        for j in range(jmax):
+            for k in range(kmax):
+                if field1[i, j, k] != 0:
+                    field0[i, j, k] /= field1[i, j, k]
