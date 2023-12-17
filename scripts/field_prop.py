e4ee9d70abf0ea43d7b9df8932c34d218bcb2604
Richard Stiskalek <richard.stiskalek@protonmail.com>
2023-12-17 12:15:33 +0100

    Add a new draft of the density calculator

--- scripts/field_prop.py
+++ scripts/field_prop.py
@@ -29,55 +29,65 @@ import csiborgtools
 from utils import get_nsims
 
 
-###############################################################################
-#                   Cosmotool SPH density & velocity field                    #
-###############################################################################
-
-def cosmotool_sph(nsim, parser_args):
-    pass
+from h5py import File
 
 
+# EVERYTHING HERE NEEDS TO BE UPDATED.
 
 ###############################################################################
 #                            Density field                                    #
 ###############################################################################
 
+# TODO do RSP separately | routines that read the particles are also old.
 
-def density_field(nsim, parser_args, to_save=True):
+
+def density_field(nsim, parser_args):
     """
     Calculate the density field in the CSiBORG simulation.
     """
     paths = csiborgtools.read.Paths(**csiborgtools.paths_glamdring)
-    nsnap = max(paths.get_snapshots(nsim, "csiborg"))
-    box = csiborgtools.read.CSiBORG1Box(nsnap, nsim, paths)
-    fname = paths.processed_output(nsim, "csiborg", "halo_catalogue")
+    nsnap = max(paths.get_snapshots(nsim, parser_args.simname))
 
-    if not parser_args.in_rsp:
-        # TODO I removed this function
-        snap = csiborgtools.read.read_h5(fname)["snapshot_final"]
-        pos = snap["pos"]
-        mass = snap["mass"]
+    boxsize = csiborgtools.simname2boxsize(parser_args.simname)
 
-        gen = csiborgtools.field.DensityField(box, parser_args.MAS)
+    fname = paths.snapshot(nsnap, nsim, parser_args.simname)
+
+    snapshot = File(fname, 'r')
+
+    gen = csiborgtools.field.DensityField(boxsize, parser_args.MAS)
+
+    if parser_args.simname == "csiborg1":
+        pos = snapshot["Coordinates"]
+        mass = snapshot["Masses"]
+
         field = gen(pos, mass, parser_args.grid, verbose=parser_args.verbose)
+    elif "csiborg2" in parser_args.simname:
+        # First do the high-resolution particles
+        pos = snapshot["PartType1"]["Coordinates"]
+        mass = numpy.ones(len(pos), dtype=pos.dtype)
+        mass *= snapshot.header.attrs["MassTable"][1]
+        field = gen(pos, mass, parser_args.grid, verbose=parser_args.verbose)
+
+        # And then the low-resolution particles
+        pos = snapshot["PartType5"]["Coordinates"]
+        mass = snapshot["PartType5"]["Masses"]
+        field += gen(pos, mass, parser_args.grid, verbose=parser_args.verbose)
+
+        # Convert to `Msun / h`
+        field *= 1e10
     else:
-        field = numpy.load(paths.field(
-            "density", parser_args.MAS, parser_args.grid, nsim, False))
-        radvel_field = numpy.load(paths.field(
-            "radvel", parser_args.MAS, parser_args.grid, nsim, False))
+        raise ValueError(f"Unknown simname: {parser_args.simname}")
 
-        if parser_args.verbose:
-            print(f"{datetime.now()}: converting density field to RSP.",
-                  flush=True)
+    # Normalize so that the field is in units of `h^2 Msun / kpc^3`
+    field /= (boxsize * 1000 / parser_args.grid)**3
 
-        field = csiborgtools.field.field2rsp(field, radvel_field, box,
-                                             parser_args.MAS)
 
-    if to_save:
-        fout = paths.field(parser_args.kind, parser_args.MAS, parser_args.grid,
-                           nsim, parser_args.in_rsp)
-        print(f"{datetime.now()}: saving output to `{fout}`.")
-        numpy.save(fout, field)
+    # TODO clear out these paths.
+    fout = paths.field(parser_args.kind, parser_args.MAS, parser_args.grid,
+                        nsim, parser_args.in_rsp)
+    print(f"{datetime.now()}: saving output to `{fout}`.")
+    # Save as .hdf5?
+    numpy.save(fout, field)
     return field
 
 
@@ -240,6 +250,48 @@ def environment_field(nsim, parser_args, to_save=True):
     return env
 
 
+def observer_peculiar_velocity(nsim, parser_args):
+    """
+    Calculate the peculiar velocity of an observer in the centre of the box
+    for several smoothing scales.
+    """
+    pos = numpy.array([0.5, 0.5, 0.5]).reshape(-1, 3)
+    boxsize = 677.7
+    smooth_scales = [0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0]
+
+    observer_vp = numpy.full((len(smooth_scales), 3), numpy.nan,
+                             dtype=numpy.float32)
+
+    paths = csiborgtools.read.Paths(**csiborgtools.paths_glamdring)
+    field_path = paths.field("velocity", parser_args.MAS, parser_args.grid,
+                             nsim, in_rsp=False)
+    field0 = numpy.load(field_path)
+
+    for j, smooth_scale in enumerate(tqdm(smooth_scales,
+                                          desc="Smoothing the fields",
+                                          disable=not parser_args.verbose)):
+        if smooth_scale > 0:
+            field = [None, None, None]
+            for k in range(3):
+                field[k] = csiborgtools.field.smoothen_field(
+                    field0[k], smooth_scale, boxsize)
+        else:
+            field = field0
+
+        v = csiborgtools.field.evaluate_cartesian(
+            field[0], field[1], field[2], pos=pos)
+        observer_vp[j, 0] = v[0][0]
+        observer_vp[j, 1] = v[1][0]
+        observer_vp[j, 2] = v[2][0]
+
+    fout = paths.observer_peculiar_velocity(parser_args.MAS, parser_args.grid,
+                                            nsim)
+    if parser_args.verbose:
+        print(f"Saving to ... `{fout}`")
+    numpy.savez(fout, smooth_scales=smooth_scales, observer_vp=observer_vp)
+    return observer_vp
+
+
 ###############################################################################
 #                          Command line interface                             #
 ###############################################################################
@@ -249,6 +301,9 @@ if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("--nsims", type=int, nargs="+", default=None,
                         help="IC realisations. `-1` for all simulations.")
+    parser.add_argument("--simname", type=str, default="csiborg",
+                        choices=["csiborg", "csiborg2"],
+                        help="Verbosity flag for reading in particles.")
     parser.add_argument("--kind", type=str,
                         choices=["density", "rspdensity", "velocity", "radvel",
                                  "potential", "environment"],
@@ -256,14 +311,11 @@ if __name__ == "__main__":
     parser.add_argument("--MAS", type=str,
                         choices=["NGP", "CIC", "TSC", "PCS"])
     parser.add_argument("--grid", type=int, help="Grid resolution.")
-    parser.add_argument("--in_rsp", type=lambda x: bool(strtobool(x)),
-                        help="Calculate in RSP?")
+    # parser.add_argument("--in_rsp", type=lambda x: bool(strtobool(x)),
+    #                     help="Calculate in RSP?")
     parser.add_argument("--smooth_scale", type=float, default=0.0,
                         help="Smoothing scale in Mpc / h. Only used for the environment field.")  # noqa
     parser.add_argument("--verbose", type=lambda x: bool(strtobool(x)),
-                        help="Verbosity flag for reading in particles.")
-    parser.add_argument("--simname", type=str, default="csiborg",
-                        choices=["csiborg", "csiborg2"],
                         help="Verbosity flag for reading in particles.")
     parser_args = parser.parse_args()
     comm = MPI.COMM_WORLD
@@ -285,3 +337,41 @@ if __name__ == "__main__":
             raise RuntimeError(f"Field {parser_args.kind} is not implemented.")
 
     work_delegation(main, nsims, comm, master_verbose=True)
+
+
+def density_field(nsim, parser_args, to_save=True):
+    """
+    Calculate the density field in the CSiBORG simulation.
+    """
+    paths = csiborgtools.read.Paths(**csiborgtools.paths_glamdring)
+    nsnap = max(paths.get_snapshots(nsim, "csiborg"))
+    box = csiborgtools.read.CSiBORG1Box(nsnap, nsim, paths)
+    fname = paths.processed_output(nsim, "csiborg", "halo_catalogue")
+
+    if not parser_args.in_rsp:
+        # TODO I removed this function
+        snap = csiborgtools.read.read_h5(fname)["snapshot_final"]
+        pos = snap["pos"]
+        mass = snap["mass"]
+
+        gen = csiborgtools.field.DensityField(box, parser_args.MAS)
+        field = gen(pos, mass, parser_args.grid, verbose=parser_args.verbose)
+    else:
+        field = numpy.load(paths.field(
+            "density", parser_args.MAS, parser_args.grid, nsim, False))
+        radvel_field = numpy.load(paths.field(
+            "radvel", parser_args.MAS, parser_args.grid, nsim, False))
+
+        if parser_args.verbose:
+            print(f"{datetime.now()}: converting density field to RSP.",
+                  flush=True)
+
+        field = csiborgtools.field.field2rsp(field, radvel_field, box,
+                                             parser_args.MAS)
+
+    if to_save:
+        fout = paths.field(parser_args.kind, parser_args.MAS, parser_args.grid,
+                           nsim, parser_args.in_rsp)
+        print(f"{datetime.now()}: saving output to `{fout}`.")
+        numpy.save(fout, field)
+    return field
