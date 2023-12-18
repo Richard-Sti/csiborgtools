88fd9464d5a694f429ced21a50a11762e019a70d
rstiskalek <richard.stiskalek@protonmail.com>
2023-12-18 13:09:03 +0000

    Edit docs

--- scripts/field_prop.py
+++ scripts/field_prop.py
@@ -12,10 +12,7 @@
 # You should have received a copy of the GNU General Public License along
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-"""
-MPI script to calculate density field-derived fields in the CSiBORG
-simulations' final snapshot.
-"""
+"""MPI script to calculate the various fields."""
 from argparse import ArgumentParser
 from datetime import datetime
 
