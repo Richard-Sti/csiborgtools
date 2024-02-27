070126715b0cc3d004054d49b5c2021520f61e1b
rstiskalek <richard.stiskalek@protonmail.com>
2024-02-27 11:40:11 +0000

    add fix bug

--- csiborgtools/field/interp.py
+++ csiborgtools/field/interp.py
@@ -194,6 +194,8 @@ def evaluate_los(*fields, sky_pos, boxsize, rmax, dr, smooth_scales=None,
     field_interp = evaluate_cartesian(*fields, pos=pos,
                                       smooth_scales=smooth_scales,
                                       verbose=verbose)
+    if len(fields) == 1:
+        field_interp = [field_interp]
 
     # Now we reshape the interpolated field to have the same shape as the
     # input `sky_pos`.
