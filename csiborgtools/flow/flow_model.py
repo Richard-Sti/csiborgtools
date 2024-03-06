dfc5035e75093f7949efbc21587a6cc47c6b4bab
rstiskalek <richard.stiskalek@protonmail.com>
2024-03-06 19:13:06 +0000

    Add Carrick and fix minor bugs

--- csiborgtools/flow/flow_model.py
+++ csiborgtools/flow/flow_model.py
@@ -29,6 +29,8 @@ from astropy.cosmology import FlatLambdaCDM
 from h5py import File
 from jax import numpy as jnp
 from tqdm import tqdm, trange
+from astropy import units as u
+from astropy.coordinates import SkyCoord
 
 from ..read import CSiBORG1Catalogue
 
@@ -38,6 +40,24 @@ SPEED_OF_LIGHT = 299792.458  # km / s
 def t():
     """Shortcut to get the current time."""
     return datetime.now().strftime("%H:%M:%S")
+
+
+def radec_to_galactic(ra, dec):
+    """
+    Convert right ascension and declination to galactic coordinates (all in
+    degrees.)
+
+    Parameters
+    ----------
+    ra, dec : float or 1-dimensional array
+        Right ascension and declination in degrees.
+
+    Returns
+    -------
+    l, b : float or 1-dimensional array
+    """
+    c = SkyCoord(ra=ra*u.degree, dec=dec*u.degree, frame='icrs')
+    return c.galactic.l.degree, c.galactic.b.degree
 
 
 ###############################################################################
@@ -56,11 +76,12 @@ class DataLoader:
         Simulation name.
     catalogue : str
         Name of the catalogue with LOS objects.
-
-    los_catalogue_fpath : str
+    catalogue_fpath : str
         Path to the LOS catalogue file.
     paths : csiborgtools.read.Paths
         Paths object.
+    ksmooth : int, optional
+        Smoothing index.
     store_full_velocity : bool, optional
         Whether to store the full 3D velocity field. Otherwise stores only
         the radial velocity.
@@ -90,13 +111,18 @@ class DataLoader:
         print(f"{t()}: calculating the radial velocity.")
         nobject, nsim = self._los_density.shape[:2]
 
+        # In case of Carrick 2015 the box is in galactic coordinates..
+        if simname == "Carrick2015":
+            d1, d2 = radec_to_galactic(self._cat["RA"], self._cat["DEC"])
+        else:
+            d1, d2 = self._cat["RA"], self._cat["DEC"]
+
         radvel = np.empty((nobject, nsim, len(self._field_rdist)),
                           self._los_velocity.dtype)
         for i in trange(nobject):
-            RA, dec = self._cat[i]["RA"], self._cat[i]["DEC"]
             for j in range(nsim):
                 radvel[i, j, :] = radial_velocity_los(
-                    self._los_velocity[i, j, ...], RA, dec)
+                    self._los_velocity[:, i, j, ...], d1[i], d2[i])
         self._los_radial_velocity = radvel
 
         if not store_full_velocity:
@@ -104,14 +130,16 @@ class DataLoader:
 
         if simname == "csiborg1":
             Omega_m = 0.307
+        elif simname == "Carrick2015":
+            Omega_m = 0.3
         else:
             raise ValueError(f"Unknown simulation: `{simname}`.")
 
-        cosmo = FlatLambdaCDM(H0=100, Om0=Omega_m)
-        mean_rho_matter = cosmo.critical_density0.to("Msun/kpc^3").value
-        mean_rho_matter *= Omega_m
-
-        self._los_density /= mean_rho_matter
+        if "csiborg" in simname:
+            cosmo = FlatLambdaCDM(H0=100, Om0=Omega_m)
+            mean_rho_matter = cosmo.critical_density0.to("Msun/kpc^3").value
+            mean_rho_matter *= Omega_m
+            self._los_density /= mean_rho_matter
 
     @property
     def cat(self):
@@ -200,11 +228,11 @@ class DataLoader:
                     out_density = np.empty(
                         (nobject, len(nsims), nstep), dtype=np.float32)
                     out_velocity = np.empty(
-                        (nobject, len(nsims), 3, nstep), dtype=np.float32)
+                        (3, nobject, len(nsims), nstep), dtype=np.float32)
 
-                slide_index = (..., k) if has_smoothed else (...)
-                out_density[:, i, :] = f[f"density_{nsim}"][slide_index]
-                out_velocity[:, i, :, :] = f[f"velocity_{nsim}"][slide_index].reshape(nobject, 3, nstep)  # noqa
+                indx = (..., k) if has_smoothed else (...)
+                out_density[:, i, :] = f[f"density_{nsim}"][indx]
+                out_velocity[:, :, i, :] = f[f"velocity_{nsim}"][indx]
 
             rdist = f[f"rdist_{nsims[0]}"][:]
 
@@ -216,7 +244,6 @@ class DataLoader:
         """
         if catalogue == "A2":
             with File(catalogue_fpath, 'r') as f:
-                print(f.keys())
                 dtype = [(key, np.float32) for key in f.keys()]
                 arr = np.empty(len(f["RA"]), dtype=dtype)
                 for key in f.keys():
@@ -423,6 +450,9 @@ def SN_PV_wcal_validation_model(los_overdensity=None, los_velocity=None,
                                 e_mB=None, e_x1=None, e_c=None,
                                 mu_xrange=None, r_xrange=None,
                                 norm_r2_xrange=None, Omega_m=None, dr=None):
+    """
+    Pass
+    """
     Vx = numpyro.sample("Vext_x", dist.Uniform(-1000, 1000))
     Vy = numpyro.sample("Vext_y", dist.Uniform(-1000, 1000))
     Vz = numpyro.sample("Vext_z", dist.Uniform(-1000, 1000))
@@ -448,7 +478,7 @@ def SN_PV_wcal_validation_model(los_overdensity=None, los_velocity=None,
         dmu = mu_xrange - mu[i]
         ptilde = norm_r2_xrange * jnp.exp(-0.5 * dmu**2 / squared_e_mu[i])
         # TODO: Add some bias
-        ptilde *= (los_overdensity[i])
+        ptilde *= (1 + los_overdensity[i])
 
         zobs_pred = predict_zobs(r_xrange, beta, Vext_rad, los_velocity[i],
                                  Omega_m)
@@ -462,47 +492,41 @@ def SN_PV_wcal_validation_model(los_overdensity=None, los_velocity=None,
 
     numpyro.factor("ll", ll)
 
-# def SN_PV_validation_model(los_overdensity=None, los_velocity=None,
-#                                 RA=None, dec=None, z_CMB=None,
-#                                 mB=None, x1=None, c=None,
-#                                 e_mB=None, e_x1=None, e_c=None,
-#                                 mu_xrange=None, r_xrange=None,
-#                                 norm_r2_xrange=None, Omega_m=None, dr=None):
-#     Vx = numpyro.sample("Vext_x", dist.Uniform(-1000, 1000))
-#     Vy = numpyro.sample("Vext_y", dist.Uniform(-1000, 1000))
-#     Vz = numpyro.sample("Vext_z", dist.Uniform(-1000, 1000))
-#     beta = numpyro.sample("beta", dist.Uniform(-10, 10))
-#
-#     # TODO: Later sample these as well.
-#     e_mu_intrinsic = 0.064
-#     alpha_cal = 0.135
-#     beta_cal = 2.9
-#     mag_cal = -18.555
-#     sigma_v = 112
-#
-#     # TODO: Check these for fiducial values.
-#     mu = mB - mag_cal + alpha_cal * x1 - beta_cal * c
-#     squared_e_mu = e_mB**2 + alpha_cal**2 * e_x1**2 + beta_cal**2 * e_c**2
-#
-#     squared_e_mu += e_mu_intrinsic**2
-#     ll = 0.
-#     for i in range(len(los_overdensity)):
-#         # Project the external velocity for this galaxy.
-#         Vext_rad = project_Vext(Vx, Vy, Vz, RA[i], dec[i])
-#
-#         dmu = mu_xrange - mu[i]
-#         ptilde = norm_r2_xrange * jnp.exp(-0.5 * dmu**2 / squared_e_mu[i])
-#         # TODO: Add some bias
-#         ptilde *= (los_overdensity[i])
-#
-#         zobs_pred = predict_zobs(r_xrange, beta, Vext_rad, los_velocity[i],
-#                                  Omega_m)
-#
-#         dczobs = SPEED_OF_LIGHT * (z_CMB[i] - zobs_pred)
-#
-#         ll_zobs = jnp.exp(-0.5 * (dczobs / sigma_v)**2) / sigma_v
-#
-#         ll += jnp.log(simps(ptilde * ll_zobs, dr))
-#         ll -= jnp.log(simps(ptilde, dr))
-#
-#     numpyro.factor("ll", ll)
+
+def SN_PV_validation_model(los_overdensity=None, los_velocity=None,
+                           RA=None, dec=None, z_obs=None,
+                           r_hMpc=None, e_r_hMpc=None,
+                           r_xrange=None, norm_r2_xrange=None, Omega_m=None,
+                           dr=None):
+    # Vx = numpyro.sample("Vext_x", dist.Uniform(-1000, 1000))
+    # Vy = numpyro.sample("Vext_y", dist.Uniform(-1000, 1000))
+    # Vz = numpyro.sample("Vext_z", dist.Uniform(-1000, 1000))
+    Vx, Vy, Vz = 0, 0, 0
+    beta = numpyro.sample("beta", dist.Uniform(-10, 10))
+
+    # TODO: Later sample these as well.
+    sigma_v = 112
+
+    ll = 0.
+    # TODO: This loop makes it incredibly slow to compile. Let's get rid of it
+    # completely and split it into small parts and vectorize it.
+    for i in range(len(los_overdensity)):
+        Vext_rad = project_Vext(Vx, Vy, Vz, RA[i], dec[i])
+
+        deltaR = r_xrange - r_hMpc[i]
+        ptilde = norm_r2_xrange * jnp.exp(-0.5 * deltaR**2 / e_r_hMpc[i]**2)
+
+        # TODO: Add some bias
+        ptilde *= (1 + los_overdensity[i])
+
+        zobs_pred = predict_zobs(r_xrange, beta, Vext_rad, los_velocity[i],
+                                 Omega_m)
+
+        dczobs = SPEED_OF_LIGHT * (z_obs[i] - zobs_pred)
+
+        ll_zobs = jnp.exp(-0.5 * (dczobs / sigma_v)**2) / sigma_v
+
+        ll += jnp.log(simps(ptilde * ll_zobs, dr))
+        ll -= jnp.log(simps(ptilde, dr))
+
+    numpyro.factor("ll", ll)
