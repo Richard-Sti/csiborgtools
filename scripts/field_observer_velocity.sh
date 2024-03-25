21a758af4e944e6169229c21e07b6e5e12c97cca
rstiskalek <richard.stiskalek@protonmail.com>
2024-03-25 15:08:45 +0000

    Add script

---
+++ scripts/field_observer_velocity.sh
@@ -0,0 +1,20 @@
+nthreads=1
+memory=40
+on_login=0
+queue="berg"
+env="/mnt/zfsusers/rstiskalek/csiborgtools/venv_csiborg/bin/python"
+file="field_observer_velocity.py"
+
+simname=${1}
+
+pythoncm="$env $file --simname $simname"
+if [ $on_login -eq 1 ]; then
+    echo $pythoncm
+    $pythoncm
+else
+    cm="addqueue -q $queue -n $nthreads -m $memory $pythoncm"
+    echo "Submitting:"
+    echo $cm
+    echo
+    eval $cm
+fi
