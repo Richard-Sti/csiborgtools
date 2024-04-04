9b45087a99a42245e9c3b12faf2e5b890a9fc33b
rstiskalek <richard.stiskalek@protonmail.com>
2024-04-04 16:17:44 +0100

    Little updates

--- scripts/mah_random.sh
+++ scripts/mah_random.sh
@@ -1,13 +1,13 @@
 memory=4
-on_login=0
-nthreads=${1}
+on_login=${1}
+nthreads=5
 
 queue="berg"
 env="/mnt/users/rstiskalek/csiborgtools/venv_csiborg/bin/python"
 file="mah_random.py"
 
 min_logmass=13.0
-simname=${2}
+simname="csiborg2_random"
 
 
 pythoncm="$env $file --simname $simname --min_logmass $min_logmass"
