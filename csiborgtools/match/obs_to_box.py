e8358a46f734dc31c50d31b14bc58d128daac635
rstiskalek <richard.stiskalek@protonmail.com>
2024-04-11 15:23:32 +0100

    Docs

--- csiborgtools/match/obs_to_box.py
+++ csiborgtools/match/obs_to_box.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2022 Richard Stiskalek
+# Copyright (C) 2024 Richard Stiskalek
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the
 # Free Software Foundation; either version 3 of the License, or (at your
