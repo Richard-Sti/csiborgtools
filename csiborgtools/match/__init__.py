4802c46069dc75d1191ec33fba5cbee099cb0cae
rstiskalek <richard.stiskalek@protonmail.com>
2024-04-11 11:16:15 +0100

    Rename file

--- csiborgtools/match/__init__.py
+++ csiborgtools/match/__init__.py
@@ -12,5 +12,5 @@
 # You should have received a copy of the GNU General Public License along
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-from .match import (ParticleOverlap, RealisationsMatcher, calculate_overlap,    # noqa
+from .overlap import (ParticleOverlap, RealisationsMatcher, calculate_overlap,  # noqa
                     find_neighbour, matching_max)                               # noqa
