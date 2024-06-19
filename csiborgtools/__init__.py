8556a918d4b9b36905ac38e56dc5d69e69551d89
rstiskalek <richard.stiskalek@protonmail.com>
2024-06-19 10:34:33 +0100

    Update imports

--- csiborgtools/__init__.py
+++ csiborgtools/__init__.py
@@ -21,7 +21,7 @@ from .utils import (center_of_mass, delta2ncells, number_counts,                
                     hms_to_degrees, dms_to_degrees, great_circle_distance,      # noqa
                     radec_to_cartesian, cartesian_to_radec,                     # noqa
                     thin_samples_by_acl, numpyro_gof, radec_to_galactic,        # noqa
-                    heliocentric_to_cmb)                                        # noqa
+                    heliocentric_to_cmb, calculate_acl)                         # noqa
 from .params import (paths_glamdring, simname2boxsize, simname2Omega_m,         # noqa
                      snap2redshift)                                             # noqa
 
