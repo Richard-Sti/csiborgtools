f602d5670b6046b4f55fcca1dc4ca3df08159c7f
rstiskalek <richard.stiskalek@protonmail.com>
2024-08-30 08:58:48 +0100

    Update numpyro selection

--- csiborgtools/flow/selection.py
+++ csiborgtools/flow/selection.py
@@ -13,10 +13,10 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 """Selection functions for peculiar velocities."""
-import numpy as np
 from jax import numpy as jnp
-from scipy.integrate import quad
-from scipy.optimize import minimize
+from numpyro import factor, sample
+from numpyro.distributions import Normal, Uniform
+from quadax import simpson
 
 
 class ToyMagnitudeSelection:
@@ -27,44 +27,43 @@ class ToyMagnitudeSelection:
     ----------
     [1] https://www.arxiv.org/abs/2408.03660
     """
-    def log_true_pdf(self, m, m1):
+
+    def __init__(self):
+        self.mrange = jnp.linspace(0, 25, 1000)
+
+    def log_true_pdf(self, m, alpha, m1):
         """Unnormalized `true' PDF."""
-        return 0.6 * (m - m1)
+        return alpha * (m - m1)
 
     def log_selection_function(self, m, m1, m2, a):
         """Logarithm of the Boubel+2024 selection function."""
-        return np.where(m <= m1,
-                        0,
-                        a * (m - m2)**2 - a * (m1 - m2)**2 - 0.6 * (m - m1))
+        return jnp.where(m <= m1,
+                         0,
+                         a * (m - m2)**2 - a * (m1 - m2)**2 - 0.6 * (m - m1))
 
-    def log_observed_pdf(self, m, m1, m2, a):
+    def log_observed_pdf(self, m, alpha, m1, m2, a):
         """
         Logarithm of the unnormalized observed PDF, which is the product
         of the true PDF and the selection function.
         """
-        # Calculate the normalization constant
-        f = lambda m: 10**(self.log_true_pdf(m, m1)                             # noqa
-                           + self.log_selection_function(m, m1, m2, a))
-        mmin, mmax = 0, 25
-        norm = quad(f, mmin, mmax)[0]
+        y = 10**(self.log_true_pdf(self.mrange, alpha, m1)
+                 + self.log_selection_function(self.mrange, m1, m2, a)
+                 )
+        norm = simpson(y, x=self.mrange)
 
-        return (self.log_true_pdf(m, m1)
+        return (self.log_true_pdf(m, alpha, m1)
                 + self.log_selection_function(m, m1, m2, a)
-                - np.log10(norm))
+                - jnp.log10(norm))
 
-    def fit(self, mag):
-        """Fit magnitude selection parameters to the observed magnitudes."""
+    def __call__(self, mag):
+        """NumPyro model, uses an informative prior on `alpha`."""
+        alpha = sample("alpha", Normal(0.6, 0.1))
+        m1 = sample("m1", Uniform(0, 25))
+        m2 = sample("m2", Uniform(0, 25))
+        a = sample("a", Uniform(-10, 0))
 
-        def loss(x):
-            m1, m2, a = x
-
-            if a >= 0:
-                return np.inf
-
-            return -np.sum(self.log_observed_pdf(mag, m1, m2, a))
-
-        x0 = [12.0, 12.5, -0.1]
-        return minimize(loss, x0, method="Nelder-Mead")
+        ll = jnp.sum(self.log_observed_pdf(mag, alpha, m1, m2, a))
+        factor("ll", ll)
 
 
 def toy_log_magnitude_selection(mag, m1, m2, a):
