8b373d79464a0e386fe4f3b0627a98f71c34974f
rstiskalek <richard.stiskalek@protonmail.com>
2024-08-31 21:45:48 +0100

    Update script

--- scripts/field_prop/field_los.sh
+++ scripts/field_prop/field_los.sh
@@ -18,12 +18,8 @@ then
 fi
 
 
-# csiborg1, csiborg2_main, csiborg2X
-
-#for simname in "Lilow2024" "CF4" ; do
-for simname in "Lilow2024"; do
-    # for catalogue in "LOSS" "Foundation" "2MTF" "SFI_gals" "CF4_GroupAll" "CF4_TFR"; do
-    for catalogue in "CF4_GroupAll"; do
+for simname in "csiborg1" "csiborg2_main" "csiborg2X" "Lilow2024" "Carrick2015" "CF4"; do
+    for catalogue in "LOSS" "Foundation" "2MTF" "SFI_gals" "CF4_GroupAll" "CF4_TFR"; do
         pythoncm="$env $file --catalogue $catalogue --nsims $nsims --simname $simname --MAS $MAS --grid $grid"
         if [ $on_login -eq 1 ]; then
             echo $pythoncm
