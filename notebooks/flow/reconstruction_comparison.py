38c96b7fd2b81465ac40188ab3f4f719b6cce936
rstiskalek <richard.stiskalek@protonmail.com>
2024-09-12 10:14:29 +0100

    Add name

--- notebooks/flow/reconstruction_comparison.py
+++ notebooks/flow/reconstruction_comparison.py
@@ -156,6 +156,7 @@ def simname_to_pretty(simname):
            "csiborg2X": "Manticore",
            "CF4": "Courtois+23",
            "CF4gp": "CF4group",
+           "CLONES": "Sorce+2018",
            }
 
     if isinstance(simname, list):
