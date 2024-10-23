ed8d609a2abef9025d595a901dbcc4d271872fd4
rstiskalek <richard.stiskalek@protonmail.com>
2024-10-23 23:03:23 +0100

    Add intrinsic h scatter

--- csiborgtools/read/paths.py
+++ csiborgtools/read/paths.py
@@ -694,7 +694,8 @@ class Paths:
                         sample_beta=False, no_Vext=None,
                         sample_Vmono=False, sample_mag_dipole=False,
                         sample_curvature=False, absolute_calibration=None,
-                        sample_Vmag_vax=False, verbose_print=True):
+                        sample_Vmag_vax=False,
+                        sample_h_intrinsic_scatter=False,verbose_print=True):
         """Flow validation file path."""
         if isinstance(catalogue, list) and len(catalogue) == 1:
             catalogue = catalogue[0]
@@ -710,11 +711,13 @@ class Paths:
         keys = ["smooth", "nsim", "zcmb_min", "zcmb_max", "mag_selection",
                 "sample_alpha", "sample_beta", "no_Vext", "sample_Vmono",
                 "sample_mag_dipole", "sample_curvature",
-                "sample_Vmag_vax", "absolute_calibration"]
+                "sample_Vmag_vax", "absolute_calibration",
+                "sample_h_intrinsic_scatter"]
         values = [smooth, nsim, zcmb_min, zcmb_max, mag_selection,
                   sample_alpha, sample_beta, no_Vext, sample_Vmono,
                   sample_mag_dipole, sample_curvature,
-                  sample_Vmag_vax, absolute_calibration]
+                  sample_Vmag_vax, absolute_calibration,
+                  sample_h_intrinsic_scatter]
 
         for key, value in zip(keys, values):
 
