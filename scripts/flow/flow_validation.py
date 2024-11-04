0b1a351845543b8a093f4a06e2d26d82fbd23ca4
rstiskalek <richard.stiskalek@protonmail.com>
2024-11-04 16:11:29 +0000

    Update submit file

--- scripts/flow/flow_validation.py
+++ scripts/flow/flow_validation.py
@@ -326,7 +326,7 @@ if __name__ == "__main__":
 
     # `None` means default behaviour
     nsteps = 1500
-    nburn = 1500
+    nburn = 10_000
     zcmb_min = None
     zcmb_max = 0.05
     nchains_harmonic = 10
