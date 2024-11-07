337f9532ec2907f3819f483d17137e151f088504
rstiskalek <richard.stiskalek@protonmail.com>
2024-11-07 15:56:50 +0000

    Update script

--- scripts/flow/flow_validation.sh
+++ scripts/flow/flow_validation.sh
@@ -4,7 +4,6 @@ on_login=${1}
 queue=${2}
 ndevice=1
 file="flow_validation.py"
-ksmooth=0
 
 
 if [ "$on_login" != "1" ] && [ "$on_login" != "0" ]
@@ -38,23 +37,31 @@ fi
 
 # For the void test: "LOSS,Foundation,CF4_TFR_i,CF4_TFR_notSDSS_w1"
 
+# for simname in "IndranilVoidSizeVar_exp"; do
 # for simname in "IndranilVoid_exp" "IndranilVoid_gauss" "IndranilVoid_mb"; do
+# for simname in "no_field"; do
 # for simname in "IndranilVoidSizeVar_exp" "IndranilVoidSizeVar_gauss" "IndranilVoidSizeVar_mb"; do
-for simname in "no_field"; do
-# for simname in "Carrick2015" "Lilow2024" "csiborg2_main" "csiborg2X" "manticore_2MPP_N128_DES_V1" "CF4" "CLONES"; do
-    # for catalogue in "LOSS" "Foundation" "2MTF" "SFI_gals" "CF4_TFR_i" "CF4_TFR_w1"; do
+# for simname in "Carrick2015"; do
+for simname in "Carrick2015" "Lilow2024" "csiborg1" "csiborg2_main" "manticore_2MPP_N128_DES_V1" "CF4" "CLONES"; do
+# for simname in "csiborg1"; do
+    # for catalogue in "LOSS,Foundation,CF4_TFR_i,CF4_TFR_notSDSS_w1"; do
+    # for catalogue in "CF4_TFR_w1"; do
+    for catalogue in "LOSS" "Foundation" "2MTF" "SFI_gals" "CF4_TFR_i" "CF4_TFR_w1"; do
+    # for catalogue in "LOSS,Foundation,CF4_TFR_i,CF4_TFR_notSDSS_w1"; do
+    # for catalogue in "LOSS,Foundation" "LOSS,Foundation,CF4_TFR_i,CF4_TFR_notSDSS_w1"; do
+    # for catalogue in "CF4_TFR_i,CF4_TFR_notSDSS_w1"; do
     # for catalogue in "CF4_TFR_i" "CF4_TFR_w1"; do
     # for catalogue in "2MTF" "SFI_gals" "CF4_TFR_i" "CF4_TFR_w1"; do
     # for catalogue in "LOSS,Foundation,CF4_TFR_i,CF4_TFR_notSDSS_w1"; do
     # for catalogue in "LOSS,Foundation,CF4_TFR_i,CF4_TFR_notSDSS_w1"; do
-    for catalogue in "LOSS,Foundation,CF4_TFR_i,CF4_TFR_notSDSS_w1"; do
+    # for catalogue in "LOSS,Foundation,CF4_TFR_i,CF4_TFR_notSDSS_w1"; do
     # for catalogue in "Carrick2MTFmock_0"; do
         for ksim in "none"; do
-        # for ksim in 0; do
         # for ksim in $(seq 0 5 500); do
         # for ksim in "0_100_5" "100_200_5" "200_300_5" "300_400_5" "400_500_5"; do
         # for ksim in {0..500}; do
             for ksmooth in 0; do
+            # for ksmooth in $(seq 0 1 33); do
                 pythoncm="$env $file --catalogue $catalogue --simname $simname --ksim $ksim --ksmooth $ksmooth --ndevice $ndevice --device $device"
 
                 if [ "$on_login" == "1" ]; then
