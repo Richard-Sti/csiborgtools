dc7e8a3a1686103208d893a1ecc795afb131d631
rstiskalek <richard.stiskalek@protonmail.com>
2024-11-18 16:50:37 +0000

    Add growth factor code

--- csiborgtools/flow/__init__.py
+++ csiborgtools/flow/__init__.py
@@ -26,3 +26,6 @@ from .void_model import (load_void_fiducial, load_void_size_variation,          
                          select_vvoid, mock_void,                               # noqa
                          # void_bulkflow_from_vrad                              # noqa
                          )                                                      # noqa
+from .growth_factor import (sigma8_nonlinear_to_linear_juszkiewicz,             # noqa
+                            make_nonlinear_to_linear_sigma8,                    # noqa
+                            find_linear_sigma8)                                 # noqa
