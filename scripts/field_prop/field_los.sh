1bea38d98ebf411bd783abd64753ef270dfdffea
rstiskalek <richard.stiskalek@protonmail.com>
2025-01-14 19:49:48 +0000

    Update script

--- scripts/field_prop/field_los.sh
+++ scripts/field_prop/field_los.sh
@@ -1,7 +1,7 @@
 nthreads=1
 memory=12
 on_login=${1}
-queue="cmb"
+queue="berg"
 env="/mnt/users/rstiskalek/csiborgtools/venv_csiborg/bin/python"
 file="field_los.py"
 
@@ -19,10 +19,10 @@ fi
 
 
 # for simname in "csiborg1" "csiborg2_main" "csiborg2X" "Lilow2024" "Carrick2015" "CF4" "manticore_2MPP_N128_DES_V1"; do
-for simname in "Carrick2015"; do
+for simname in "CF4"; do
 # for simname in "csiborg1" "csiborg2_main" "Lilow2024" "Carrick2015" "CF4" "CLONES"; do
-    # for catalogue in "LOSS" "Foundation" "2MTF" "SFI_gals" "CF4_TFR"; do
-    for catalogue in "Pantheon+"; do
+    for catalogue in "LOSS" "Foundation" "2MTF" "SFI_gals" "CF4_TFR"; do
+    # for catalogue in "Pantheon+"; do
     # for catalogue in "LOSS"; do
     # for catalogue in "SDSS-FP"; do
         pythoncm="$env $file --catalogue $catalogue --nsims $nsims --simname $simname --MAS $MAS --grid $grid"
