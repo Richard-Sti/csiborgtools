5f01a84f91bbc711adb4904403dcbf08f64fb6c4
rstiskalek <richard.stiskalek@protonmail.com>
2025-01-15 00:20:00 +0000

    Update script

--- scripts/flow/flow_validation.sh
+++ scripts/flow/flow_validation.sh
@@ -22,8 +22,8 @@ fi
 if [ "$queue" == "gpulong" ]
 then
     device="gpu"
-    gputype="rtx2080with12gb"
-    # gputype="rtx3070with8gb"
+    # gputype="rtx2080with12gb"
+    gputype="rtx3070with8gb"
     # gputype="rtxa6000with48gb"
     env="/mnt/users/rstiskalek/csiborgtools/venv_gpu_csiborgtools/bin/python"
 elif [ "$queue" == "cmbgpu" ]
@@ -36,57 +36,58 @@ else
     env="/mnt/users/rstiskalek/csiborgtools/venv_csiborg/bin/python"
 fi
 
-# # for simname in "IndranilVoidSizeVar_exp"; do
-# for simname in "IndranilVoid_exp"; do
-# # for simname in "no_field"; do
-# # for simname in "Carrick2015" "Lilow2024" "csiborg1" "csiborg2_main" "CF4" "CLONES"; do
-# # for simname in "manticore_2MPP_MULTIBIN_N128_DES_V1"; do
-#     # for catalogue in "CF4_TFR_i,CF4_TFR_notSDSS_w1"; do
-#     # for catalogue in "CF4_TFR_w1"; do
-#     # for catalogue in "LOSS" "Foundation" "2MTF" "SFI_gals" "CF4_TFR_i" "CF4_TFR_w1"; do
-#     # for catalogue in "Foundation" "2MTF" "SFI_gals" "CF4_TFR_i" "CF4_TFR_w1"; do
-#     for catalogue in "Pantheon+"; do
-#     # for catalogue in "CF4_GroupAll"; do
-#     # for catalogue in "LOSS,Foundation,CF4_TFR_i,CF4_TFR_notSDSS_w1"; do
-#     # for catalogue in "LOSS"; do
-#     # for catalogue in "LOSS,Foundation,CF4_TFR_i,CF4_TFR_notSDSS_w1"; do
-#     # for catalogue in "CF4_TFR_i,CF4_TFR_notSDSS_w1"; do
-#     # for catalogue in "CF4_TFR_i" "CF4_TFR_w1"; do
-#     # for catalogue in "2MTF"; do
-#     # for catalogue in "2MTF" "SFI_gals" "CF4_TFR_i" "CF4_TFR_w1"; do
-#     # for catalogue in "LOSS,Foundation,CF4_TFR_i,CF4_TFR_notSDSS_w1"; do
-#     # for catalogue in "LOSS,Foundation,CF4_TFR_i,CF4_TFR_notSDSS_w1"; do
-#     # for catalogue in "LOSS,Foundation,CF4_TFR_i,CF4_TFR_notSDSS_w1"; do
-#     # for catalogue in "Carrick2MTFmock_0"; do
-#         for ksim in "none"; do
-#         # for ksim in $(seq 0 5 500); do
-#         # for ksim in "0_100_5" "100_200_5" "200_300_5" "300_400_5" "400_500_5"; do
-#         # for ksim in {0..500}; do
-#             for ksmooth in 0; do
-#             # for ksmooth in $(seq 0 1 33); do
-#                 pythoncm="$env $file --catalogue $catalogue --simname $simname --ksim $ksim --ksmooth $ksmooth --ndevice $ndevice --device $device"
+# for simname in "IndranilVoid_gauss"; do
+# for simname in "IndranilVoidSizeVar_exp"; do
+for simname in "CF4"; do
+# for simname in "Carrick2015" "Lilow2024" "csiborg1" "csiborg2_main" "CF4" "CLONES"; do
+# for simname in "manticore_2MPP_MULTIBIN_N128_DES_V1"; do
+    # for catalogue in "CF4_TFR_i,CF4_TFR_notSDSS_w1"; do
+    # for catalogue in "CF4_TFR_w1"; do
+    # for catalogue in "LOSS" "Foundation" "2MTF" "SFI_gals" "CF4_TFR_i" "CF4_TFR_w1"; do
+    # for catalogue in "Foundation" "2MTF" "SFI_gals" "CF4_TFR_i" "CF4_TFR_w1"; do
+    # for catalogue in "Pantheon+"; do
+    # for catalogue in "CF4_GroupAll"; do
+    # for catalogue in "CF4_TFR_i,CF4_TFR_notSDSS_w1"; do
+    for catalogue in "2MTF"; do
+    # for catalogue in "LOSS"; do
+    # for catalogue in "LOSS,Foundation,CF4_TFR_i,CF4_TFR_notSDSS_w1"; do
+    # for catalogue in "CF4_TFR_i,CF4_TFR_notSDSS_w1"; do
+    # for catalogue in "CF4_TFR_i" "CF4_TFR_w1"; do
+    # for catalogue in "2MTF"; do
+    # for catalogue in "2MTF" "SFI_gals" "CF4_TFR_i" "CF4_TFR_w1"; do
+    # for catalogue in "LOSS,Foundation,CF4_TFR_i,CF4_TFR_notSDSS_w1"; do
+    # for catalogue in "LOSS,Foundation,CF4_TFR_i,CF4_TFR_notSDSS_w1"; do
+    # for catalogue in "LOSS,Foundation,CF4_TFR_i,CF4_TFR_notSDSS_w1"; do
+    # for catalogue in "Carrick2MTFmock_0"; do
+        for ksim in "none"; do
+        # for ksim in $(seq 0 5 500); do
+        # for ksim in "0_100_5" "100_200_5" "200_300_5" "300_400_5" "400_500_5"; do
+        # for ksim in {0..500}; do
+            for ksmooth in 0; do
+            # for ksmooth in $(seq 0 1 33); do
+                pythoncm="$env $file --catalogue $catalogue --simname $simname --ksim $ksim --ksmooth $ksmooth --ndevice $ndevice --device $device"
 
-#                 if [ "$on_login" == "1" ]; then
-#                     echo $pythoncm
-#                     eval $pythoncm
-#                 else
-#                     if [ "$device" == "gpu" ]; then
-#                         cm="addqueue -q $queue -s -m $memory --gpus 1 --gputype $gputype $pythoncm"
-#                     else
-#                         cm="addqueue -s -q $queue -n 1 -m $memory $pythoncm"
-#                     fi
-#                     echo "Submitting:"
-#                     echo $cm
-#                     eval $cm
-#                 fi
+                if [ "$on_login" == "1" ]; then
+                    echo $pythoncm
+                    eval $pythoncm
+                else
+                    if [ "$device" == "gpu" ]; then
+                        cm="addqueue -q $queue -s -m $memory --gpus 1 --gputype $gputype $pythoncm"
+                    else
+                        cm="addqueue -s -q $queue -n 1 -m $memory $pythoncm"
+                    fi
+                    echo "Submitting:"
+                    echo $cm
+                    eval $cm
+                fi
 
-#                 echo
-#                 sleep 0.001
+                echo
+                sleep 0.001
 
-#             done
-#         done
-#     done
-# done
+            done
+        done
+    done
+done
 
 
 ###############################################################################
@@ -94,31 +95,31 @@ fi
 ###############################################################################
 
 
-ksim="none"
-catalogue="LOSS,Foundation,CF4_TFR_i,CF4_TFR_notSDSS_w1"
-ksmooth=0
+# ksim="none"
+# catalogue="LOSS,Foundation,CF4_TFR_i,CF4_TFR_notSDSS_w1"
+# ksmooth=0
 
-# for i in {10..300..10}; do
-for i in 150; do
-    simname="IndranilVoid${i}_exp"
-    echo "$simname"
+# # for i in {10..300..10}; do
+# for i in {1..20..1}; do
+#     simname="IndranilVoid${i}_gauss"
+#     echo "$simname"
 
-    pythoncm="$env $file --catalogue $catalogue --simname $simname --ksim $ksim --ksmooth $ksmooth --ndevice $ndevice --device $device"
+#     pythoncm="$env $file --catalogue $catalogue --simname $simname --ksim $ksim --ksmooth $ksmooth --ndevice $ndevice --device $device"
 
-    if [ "$on_login" == "1" ]; then
-        echo $pythoncm
-        eval $pythoncm
-    else
-        if [ "$device" == "gpu" ]; then
-            cm="addqueue -q $queue -s -m $memory --gpus 1 --gputype $gputype $pythoncm"
-        else
-            cm="addqueue -s -q $queue -n 1 -m $memory $pythoncm"
-        fi
-        echo "Submitting:"
-        echo $cm
-        eval $cm
-    fi
+#     if [ "$on_login" == "1" ]; then
+#         echo $pythoncm
+#         eval $pythoncm
+#     else
+#         if [ "$device" == "gpu" ]; then
+#             cm="addqueue -q $queue -s -m $memory --gpus 1 --gputype $gputype $pythoncm"
+#         else
+#             cm="addqueue -s -q $queue -n 1 -m $memory $pythoncm"
+#         fi
+#         echo "Submitting:"
+#         echo $cm
+#         eval $cm
+#     fi
 
-    echo
-    sleep 0.001
-done
+#     echo
+#     sleep 0.001
+# done
