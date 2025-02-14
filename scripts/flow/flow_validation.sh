c92e8123c9c28174dececdfef7b6e731c10cb017
rstiskalek <richard.stiskalek@protonmail.com>
2025-02-14 02:37:18 +0000

    Update script

--- scripts/flow/flow_validation.sh
+++ scripts/flow/flow_validation.sh
@@ -22,8 +22,8 @@ fi
 if [ "$queue" == "gpulong" ]
 then
     device="gpu"
-    # gputype="rtx2080with12gb"
-    gputype="rtx3070with8gb"
+    gputype="rtx2080with12gb"
+    # gputype="rtx3070with8gb"
     # gputype="rtxa6000with48gb"
     env="/mnt/users/rstiskalek/csiborgtools/venv_gpu_csiborgtools/bin/python"
 elif [ "$queue" == "cmbgpu" ]
@@ -37,25 +37,29 @@ else
 fi
 
 
-aux_name="none"
+aux_name="inference_method"
 aux_type="str"
 
-# for simname in "IndranilVoid_gauss"; do
-# for simname in "IndranilVoidSizeVar_exp"; do
-for simname in "manticore_2MPP_MULTIBIN_N256_DES_V2"; do
+# for simname in "IndranilVoid_exp" "IndranilVoid_gauss"; do
+for simname in "IndranilVoidSizeVar_exp" "IndranilVoidSizeVar_gauss"; do
+# for simname in "IndranilVoid_exp"; do
 # for simname in "Carrick2015"; do
+# for simname in "IndranilVoidSizeVar_exp" "IndranilVoidSizeVar_gauss"; do
+# for simname in "manticore_2MPP_MULTIBIN_N256_DES_V2"; do
+# for simname in "CF4" "manticore_2MPP_MULTIBIN_N256_DES_V2"; do
 # for simname in "Carrick2015" "Lilow2024" "csiborg1" "csiborg2_main" "CF4" "CLONES"; do
 # for simname in "manticore_2MPP_MULTIBIN_N128_DES_V1"; do
     # for catalogue in "LOSS" "Foundation" "2MTF" "SFI_gals" "CF4_TFR_i" "CF4_TFR_w1"; do
-    # for catalogue in "CF4_TFR_i,CF4_TFR_notSDSS_w1"; do
-    for catalogue in "CF4_TFR_w1"; do
+    for catalogue in "CF4_TFR_i,CF4_TFR_notSDSS_w1"; do
+    # for catalogue in "Carrick2MTFmock_1"; do
+    # for catalogue in "SFI_gals" "2MTF" "CF4_TFR_w1" "CF4_TFR_w2" "CF4_TFR_i"; do
         for ksim in "none"; do
         # for ksim in $(seq 0 5 500); do
         # for ksim in "0_100_5" "100_200_5" "200_300_5" "300_400_5" "400_500_5"; do
         # for ksim in {0..500}; do
             for ksmooth in 0; do
             # for ksmooth in $(seq 0 1 33); do
-                for aux_arg in "Planck2013"; do
+                for aux_arg in "mike" "bayes"; do
                     pythoncm="$env $file --catalogue $catalogue --simname $simname --ksim $ksim --ksmooth $ksmooth --ndevice $ndevice --device $device --aux_name $aux_name --aux_arg $aux_arg --aux_type $aux_type"
 
                     if [ "$on_login" == "1" ]; then
