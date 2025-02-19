111f5f21ba78672759b6f4d51d57bcb8b93ef4f9
rstiskalek <richard.stiskalek@protonmail.com>
2025-02-19 03:33:46 +0000

    Update script

--- scripts/flow/flow_validation.sh
+++ scripts/flow/flow_validation.sh
@@ -38,10 +38,10 @@ fi
 
 
 aux_name="none"
-aux_type="bool"
+aux_type="str"
 
 # for simname in "IndranilVoid_exp" "IndranilVoid_gauss"; do
-for simname in "IndranilVoid_gauss"; do
+for simname in "manticore_2MPP_MULTIBIN_N256_DES_V2"; do
 # for simname in "IndranilVoidSizeVar_gauss"; do
 # for simname in "IndranilVoidSizeVar_exp" "IndranilVoidSizeVar_gauss"; do
 # for simname in "IndranilVoidSizeVar_exp"; do
@@ -53,11 +53,11 @@ for simname in "IndranilVoid_gauss"; do
 # for simname in "CF4" "manticore_2MPP_MULTIBIN_N256_DES_V2"; do
 # for simname in "Carrick2015" "Lilow2024" "csiborg1" "csiborg2_main" "CF4" "CLONES"; do
 # for simname in "manticore_2MPP_MULTIBIN_N128_DES_V1"; do
-    # for catalogue in "LOSS" "Foundation" "2MTF" "SFI_gals" "CF4_TFR_i" "CF4_TFR_w1"; do
+    for catalogue in "LOSS" "Foundation" "2MTF" "SFI_gals" "CF4_TFR_i" "CF4_TFR_w1"; do
     # for catalogue in "CF4_TFR_i,CF4_TFR_notSDSS_w1"; do
     # for catalogue in "CF4_TFR_w1"; do
     # for catalogue in "2MTF"; do
-    for catalogue in "IndranilVoidTFRMock_0"; do
+    # for catalogue in "LOSS"; do
     # for catalogue in "Carrick2MTFmock_0"; do
     # for catalogue in "SFI_gals" "2MTF" "CF4_TFR_w1" "CF4_TFR_w2" "CF4_TFR_i"; do
         for ksim in "none"; do
@@ -66,7 +66,7 @@ for simname in "IndranilVoid_gauss"; do
         # for ksim in {0..500}; do
             for ksmooth in 0; do
             # for ksmooth in $(seq 0 1 33); do
-                for aux_arg in "0"; do
+                for aux_arg in "bayes"; do
                     pythoncm="$env $file --catalogue $catalogue --simname $simname --ksim $ksim --ksmooth $ksmooth --ndevice $ndevice --device $device --aux_name $aux_name --aux_arg $aux_arg --aux_type $aux_type"
 
                     if [ "$on_login" == "1" ]; then
