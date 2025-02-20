667102298e5d0385f39e979938c9e8f1af52991f
rstiskalek <richard.stiskalek@protonmail.com>
2025-02-20 02:44:26 +0000

    More updates to priors

--- scripts/flow/flow_validation.py
+++ scripts/flow/flow_validation.py
@@ -376,7 +376,7 @@ if __name__ == "__main__":
     ###########################################################################
 
     # `None` means default behaviour
-    nsteps = 10_000
+    nsteps = 15_000
     nburn = 1500
     zcmb_min = None
     # zcmb_max = 0.05
