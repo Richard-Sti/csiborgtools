58d21f44e5f26b147163d5db03cc8bc42fc9152d
rstiskalek <richard.stiskalek@protonmail.com>
2025-03-02 21:52:06 +0000

    Update script

--- scripts_independent/flow_debugging/test_TFR.py
+++ scripts_independent/flow_debugging/test_TFR.py
@@ -1,15 +1,18 @@
 from argparse import ArgumentParser
 from math import ceil
+from pathlib import Path
 
 import matplotlib.pyplot as plt
 import numpy as np
 from corner import corner
+from h5py import File
 from jax import numpy as jnp
 from jax import random
 from numpyro import factor, plate, sample
 from numpyro.distributions import Normal, Uniform
 from numpyro.infer import MCMC, NUTS
 from numpyro.infer.initialization import init_to_median
+from scipy.stats import norm
 
 SPEED_OF_LIGHT = 299_792.458
 
@@ -106,18 +109,37 @@ def generate_mock_data(injected_parameters, run_num, verbose=True,
             fig.delaxes(axes[j])
 
         fig.tight_layout()
-        fname = f"./plots/mock_data_{run_num}.png"
+        fname = f"./plots/run_{run_num}_mock_data.png"
         if verbose:
             print(f"Saving the mock data plot to `{fname}`.")
         fig.savefig(fname, dpi=450)
         plt.close()
 
-    return eta_obs, mag_obs, phi, theta, zobs
+    all_data = {
+        "eta_true": eta_true,
+        "eta_obs": eta_obs,
+        "M": M,
+        "phi": phi,
+        "theta": theta,
+        "mag_obs": mag_obs,
+        "dist": dist,
+        "Vrad": Vrad,
+        "dist": dist,
+        "ztrue": ztrue,
+        "zobs": zobs,
+    }
+
+    model_data = (eta_obs, mag_obs, phi, theta, zobs)
+
+    return model_data, all_data
 
 
-def model(obs_data, injected_params,):
+def model(obs_data, injected_params, sample_sigmaTFR, sample_sigma_v):
     eta_obs, mag_obs, phi, theta, zobs = obs_data
     ngal = len(eta_obs)
+
+    eta_mean = injected_params["eta_mean"]
+    eta_std = injected_params["eta_std"]
 
     eta_mean_min = injected_params["eta_mean"] - 1
     eta_mean_max = injected_params["eta_mean"] + 1
@@ -127,23 +149,39 @@ def model(obs_data, injected_params,):
     factor("ll_eta_std", -jnp.log(eta_std))
 
     with plate("plate_eta", ngal):
-        eta_true = sample("eta_true", Normal(eta_mean, eta_std), obs=eta_obs)
+        eta_true = sample("eta_true", Normal(eta_mean, eta_std))
+    factor(
+        "ll_eta",
+        Normal(eta_true, injected_params["e_eta"]).log_prob(eta_obs)
+        )
+
+    if sample_sigmaTFR:
+        sigmaTFR = sample(
+            "sigmaTFR", Uniform(0, 5 * injected_params["sigmaTFR"]))
+        factor("ll_sigma_TFR", -jnp.log(sigmaTFR))
+    else:
+        sigmaTFR = injected_params["sigmaTFR"]
 
     # TODO: here assume known TFR parameters
     with plate("plate_M", ngal):
         M = sample(
             "M", Normal(
                 injected_params["aTFR"] + injected_params["bTFR"] * eta_true,
-                injected_params["sigmaTFR"]))
+                sigmaTFR))
 
+    # It is important that the prior range here matches the range used to
+    # generate the mock data.
     with plate("plate_dist", ngal):
-        dist = sample("dist", Uniform(0, 2 * injected_params["dist_max"]))
+        dist = sample(
+            "dist",
+            Uniform(injected_params["dist_min"], injected_params["dist_max"]))
 
     distmod = dist2distmod(dist)
 
     with plate("plate_mag", ngal):
-        sample("mag_true", Normal(
-            M + distmod, injected_params["e_mag"]), obs=mag_obs)
+        sample(
+            "mag_true", Normal(M + distmod, injected_params["e_mag"]),
+            obs=mag_obs)
 
     Vdip_mag = sample("Vdip_mag", Uniform(0, 10 * injected_params["Vdip_mag"]))
     Vdip_ra = sample("Vdip_ra", Uniform(0, 2 * np.pi))
@@ -156,8 +194,11 @@ def model(obs_data, injected_params,):
 
     zpred = (1 + dist2redshift(dist)) * (1 + Vpec / SPEED_OF_LIGHT) - 1
 
-    sigma_v = sample("sigma_v", Uniform(0, 5 * injected_params["sigma_v"]))
-    factor("ll_sigma_v", -jnp.log(sigma_v))
+    if sample_sigma_v:
+        sigma_v = sample("sigma_v", Uniform(0, 5 * injected_params["sigma_v"]))
+        factor("ll_sigma_v", -jnp.log(sigma_v))
+    else:
+        sigma_v = injected_params["sigma_v"]
 
     with plate("plate_zobs", ngal):
         sample("zobs", Normal(zpred, sigma_v / SPEED_OF_LIGHT), obs=zobs)
@@ -174,7 +215,7 @@ def plot_corner(mcmc_samples, injected_params, params_to_plot, run_num,
         samples, labels=params_to_plot, show_titles=True, truths=truths,
         truth_color="red", title_kwargs={"fontsize": 12}, smooth=1)
 
-    fname = f"./plots/corner_{run_num}.png"
+    fname = f"./plots/run_{run_num}.png"
     if verbose:
         print(f"Saving the corner plot to `{fname}`.")
     fig.savefig(fname, dpi=450)
@@ -187,10 +228,13 @@ if __name__ == "__main__":
     argparser.add_argument("run_num", type=int, help="Run number")
     args = argparser.parse_args()
 
-    nwarm, nsamp = 1500, 3000
+    Path("./plots").mkdir(parents=True, exist_ok=True,)
+    Path("./data").mkdir(parents=True, exist_ok=True,)
+
+    nwarm, nsamp = 1500, 5000
 
     injected_params = {
-        "ngal": 500,
+        "ngal": 2000,
         "dist_min": 30,
         "dist_max": 150,
 
@@ -209,23 +253,77 @@ if __name__ == "__main__":
 
         "e_mag": 0.05,
     }
+    sample_sigmaTFR = False
+    sample_sigma_v = True
 
     params_plot = ["Vdip_mag", "Vdip_ra", "Vdip_cos_theta",
-                   "sigma_v", "eta_mean", "eta_std"]
+                   "sigma_v", "eta_mean", "eta_std", "sigmaTFR",]
 
-    obs_data = generate_mock_data(injected_params, args.run_num)
+    model_data, all_data = generate_mock_data(injected_params, args.run_num)
 
     rng_key = random.PRNGKey(args.run_num)
     kernel = NUTS(model, init_strategy=init_to_median(num_samples=1000))
     mcmc = MCMC(kernel, num_warmup=nwarm, num_samples=nsamp,)
-    mcmc.run(rng_key, obs_data, injected_params)
+    mcmc.run(rng_key, model_data, injected_params, sample_sigmaTFR,
+             sample_sigma_v)
 
-    samples = mcmc.get_samples()
-    print(f"Sampled parameters are: {list(samples.keys())}")
-    for key in samples:
-        x = samples[key]
+    mcmc_samples = mcmc.get_samples()
+    print(f"Sampled parameters are: {list(mcmc_samples.keys())}")
+    for key in mcmc_samples:
+        x = mcmc_samples[key]
         if x.ndim > 1:
             continue
         print(f"{key:20s}: {x.mean():.6g} ± {x.std():.6g}")
 
-    plot_corner(samples, injected_params, params_plot, run_num=args.run_num)
+    plot_corner(mcmc_samples, injected_params, params_plot,
+                run_num=args.run_num)
+
+    plot_diff = {
+        r"$\Delta M / \sigma_M$": (mcmc_samples["M"], all_data["M"], True, 1),
+        r"$\Delta d / \sigma_d$": (mcmc_samples["dist"], all_data["dist"], True, 1),  # noqa
+        r"$\Delta d$": (mcmc_samples["dist"], all_data["dist"], False, None),
+    }
+
+    if len(plot_diff) > 0:
+        cols = 3
+        rows = ceil(len(plot_diff) / cols)
+
+        fig, axes = plt.subplots(rows, cols, figsize=(cols * 4, rows * 3))
+        axes = axes.flatten()
+
+        for i, (label, (samples, true, norm_bias, bias_std)) in enumerate(plot_diff.items()):  # noqa
+            ax = axes[i]
+            mu, std = np.mean(samples, axis=0), np.std(samples, axis=0)
+            if norm_bias:
+                dx = (mu - true) / std
+            else:
+                dx = mu - true
+
+            ax.hist(dx, bins="auto", alpha=0.7, edgecolor='black', density=1)
+            if bias_std is not None:
+                x = np.linspace(dx.min(), dx.max(), 1000)
+                ax.plot(x, norm(0, bias_std).pdf(x), color="k", )
+            ax.axvline(0, color="red", linestyle="--")
+            ax.set_xlabel(label)
+            ax.set_ylabel("Binned counts")
+
+        # Hide unused subplots (if any)
+        for j in range(i + 1, len(axes)):
+            fig.delaxes(axes[j])
+
+        fig.tight_layout()
+        fname = f"./plots/run_{args.run_num}_samples_diff.png"
+        print(f"Saving the mock data plot to `{fname}`.")
+        fig.savefig(fname, dpi=450)
+        plt.close()
+
+    fname = f"./data/run_{args.run_num}.hdf5"
+    print(f"Saving the data to `{fname}`.")
+    with File(fname, 'w') as f:
+        grp = f.create_group("data")
+        for key, value in all_data.items():
+            grp.create_dataset(key, data=value)
+
+        grp = f.create_group("samples")
+        for key, value in mcmc_samples.items():
+            grp.create_dataset(key, data=value)
